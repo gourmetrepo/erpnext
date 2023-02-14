503b78d6d560c5f9a75e835cad95723f726dfc1f
umairqasim123 <121786049+umairqasim123@users.noreply.github.com>
2023-02-14 10:17:58 +0500

    Returnable total quantity
    Erpnext reordered fields in delivery note doctype and added new field returnable total quantity.

--- erpnext/stock/doctype/delivery_note/delivery_note.py
+++ erpnext/stock/doctype/delivery_note/delivery_note.py
@@ -202,6 +202,13 @@ class DeliveryNote(SellingController):
 		## id returnable
 		if self.is_return:
 			return
+		# umair added returnable total quantity
+		returnables = self.get("returnable_items")
+		returnable_total_quantity = 0.0
+		for returnable in returnables:
+			returnable_total_quantity += returnable.actual_qty
+			
+		self.returnable_total_quantity = returnable_total_quantity
 
 		if self.get('manually_manage_return_items'):
 			returnables = self.get("returnable_items")
