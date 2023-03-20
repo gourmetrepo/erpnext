d21c97125c489a83ef178e0414896487c00dde4c
Abubakar Sohail <abubakarsohail66@gmail.com>
2023-03-20 19:17:20 +0500

    exception handling in calculate_segment_profit API
    exception handling in calculate_segment_profit API

--- erpnext/accounts/doctype/account_segment_data/account_segment_data.py
+++ erpnext/accounts/doctype/account_segment_data/account_segment_data.py
@@ -52,34 +52,31 @@ def get_unit_sales_bgroup(unit,date):
 	group by `business_group`
 	""".format(date,unit),as_dict=True)
 	
-
-
 def get_segment_gl_data(account_title,account,date,unit,sale_bgroup):
-	condition = ''
-	if len(account) == 1:
-		condition = (f"""('{account[0]}')""")
-	else:
-		condition = tuple(account)
-	if len(account) == 0:
-		return
-	data = frappe.db.sql("""
+    condition = ''
+    if len(account) == 1:
+        condition = (f"""('{account[0]}')""")
+    else:
+        condition = tuple(account)
+    if len(account) == 0:
+        return
+    data = frappe.db.sql("""
 		SELECT SUM(`credit_in_account_currency`-`debit_in_account_currency`) AS account_value 
 		FROM `tabGL Entry` WHERE account IN {0}  AND company='{2}' 
 		AND DATE(creation) BETWEEN '{1}' AND '{1}'
 		""".format(condition,date,unit),as_dict=True)
-	
-	for bgroup_data in sale_bgroup:		
-		if data:
-			if data[0].account_value != None:
-				Amount = (data[0].account_value * bgroup_data.get('amount'))/100
-			else:
-				Amount = 0
-		else:
-			Amount = 0
-				#business group wise sale_bgroup (account_value*busines group amount/100) 
-				# put in bussiness group as segment in data base 
-
-		save_doc = {
+    for bgroup_data in sale_bgroup:
+        try:
+            if data:
+                if data[0].account_value != None:
+                    Amount = (data[0].account_value * bgroup_data.get('amount'))/100
+                else:
+                    Amount = 0
+            else:
+                Amount = 0
+        except Exception as e:
+            Amount = 0
+        save_doc = {
 			'doctype':'Account Segment Data',
 			'segment':bgroup_data.get('business_group'),
 			'account':account_title,
@@ -88,7 +85,6 @@ def get_segment_gl_data(account_title,account,date,unit,sale_bgroup):
 			'date':date,
 			'account_value':Amount
 		}
-		# print(save_doc)
-		data = frappe.get_doc(save_doc).save(ignore_permissions=True)
-		frappe.db.commit()
+        data = frappe.get_doc(save_doc).save(ignore_permissions=True)
+        frappe.db.commit()
 		# print('End data')
