a341364e7d6ab62abb4d5857cc75baa44a389d4d
abdulsamad2929 <asramzani@gmail.com>
2023-03-24 16:56:14 +0500

    change in P&L segment report

--- erpnext/accounts/report/segment_wise_profit_csd/segment_wise_profit_csd.py
+++ erpnext/accounts/report/segment_wise_profit_csd/segment_wise_profit_csd.py
@@ -20,19 +20,21 @@ def execute(filters=None):
 		("Other") + ":Float:180"
     ]
 	data = []
+	from_date='2023-03-01'
+	to_date='2023-03-05'
 	data = frappe.db.sql(""" 
-		SELECT `account`,
-			SUM(IF (`segment`='CSD (Carbonated Soft Drinks)' , `account_value`,0)) as csd, 
-			SUM(IF (`segment`='Juice' , `account_value` ,0))as juices,
+		SELECT head,`account`,
+			SUM(IF (`segment`='CSD (Carbonated Soft Drinks)' , `account_value`,0)) AS csd, 
+			SUM(IF (`segment`='Juice' , `account_value` ,0))AS juices,
 			SUM(IF (`segment`='Water' , `account_value`,0))AS  water,
 			SUM(IF (`segment`='Confectionery' , `account_value`,0)) AS  candyconfectionary,
 			SUM(IF (`segment`='Concentrate' , `account_value` ,0)) AS concentrates,
-			SUM(IF (`segment`='19 Ltr'  ,`account_value`,0) ) as 19ltr,
-			SUM(IF (`segment`='Other' , `account_value` ,0)) as other
+			SUM(IF (`segment`='19 Ltr'  ,`account_value`,0) ) AS 19ltr,
+			SUM(IF (`segment`='Other' , `account_value` ,0)) AS other
 		FROM `tabAccount Segment Data`
 		WHERE date BETWEEN '{from_date}' and '{to_date}' 
 		AND company = '{company}'
-			group by `account`
+			group by head,`account`
 	""".format(from_date=from_date, to_date=to_date,company=company), as_dict=True)
 	
 	return columns, data
