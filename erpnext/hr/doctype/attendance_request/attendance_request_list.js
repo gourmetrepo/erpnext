bd5f4b35ef0dc9ee822ce04b2284dc4b03e8bcb2
abdulsamad2929 <asramzani@gmail.com>
2023-03-27 13:11:10 +0500

    change in filter

--- erpnext/hr/doctype/attendance_request/attendance_request_list.js
+++ erpnext/hr/doctype/attendance_request/attendance_request_list.js
@@ -3,7 +3,7 @@ frappe.listview_settings['Attendance Request'] = {
 		if (Object.values(frappe.route_options).length == 0){
 			frappe.route_options = {
 				// "status": "Draft",
-				"employee": frappe.get_cookie("user_id") ,
+				"employee_name": frappe.get_cookie('full_name'),
 				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -5),frappe.datetime.get_today()]]
 			};
 		}
