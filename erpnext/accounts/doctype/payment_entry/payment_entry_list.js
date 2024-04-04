684c41357df93048414370d0925dd2e2458c2b9a
abdulsamad2929 <asramzani@gmail.com>
2024-04-04 10:23:56 +0500

    Change list view JS for PE & JV

--- erpnext/accounts/doctype/payment_entry/payment_entry_list.js
+++ erpnext/accounts/doctype/payment_entry/payment_entry_list.js
@@ -1,6 +1,12 @@
 frappe.listview_settings['Payment Entry'] = {
 
 	onload: function(listview) {
+			if (Object.values(frappe.route_options).length == 0){
+			frappe.route_options = {
+				"company": frappe.get_cookie('company') ,
+				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -10),frappe.datetime.get_today()]]
+			};
+		}
 		if (listview.page.fields_dict.party_type) {
 			listview.page.fields_dict.party_type.get_query = function() {
 				return {
