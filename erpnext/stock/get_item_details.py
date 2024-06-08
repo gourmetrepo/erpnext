d3245c969aceb86042a5b3e5e0447d006a16c491
Shoaib Rehmat <86551638+shoaibrehmat88@users.noreply.github.com>
2024-06-08 17:36:01 +0500

    change in query for item buying

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -583,19 +583,21 @@ def get_price_list_rate(args, item_doc, out):
 		oblige_rate = 0
   	#daily items list
 		daily_rate_item = get_config_by_name("DAILY_RATE_ITEMS", [])
-		if (args.parenttype == 'Purchase Order' or args.doctype == 'Purchase Order') and item_doc.name in daily_rate_item and args.company=='Unit 6':
+		if (args.parenttype == 'Purchase Order' or args.doctype == 'Purchase Order') and item_doc.name in daily_rate_item:
 			# oblige_rate = flt(frappe.db.get_value('Item Daily Rate Table', {
             #                 'category':'Daily Rate','company':args.company, 'item_code': item_doc.name, 'supplier_code': args.supplier, 'docstatus': '1', 'date': ["<=", frappe.utils.now()]}, 'new_rate'))
 			rate = frappe.db.sql("""
-				SELECT new_rate FROM `tabItem Daily Rate Table`
+				SELECT idrt.new_rate 
+					FROM `tabItem Daily Rate Table` AS idrt
+					JOIN `tabItem Daily Rate` AS idr ON idr.name = idrt.parent
 				WHERE
-					category='Daily Rate'
-					AND company = %s
-					AND item_code = %s
-					AND supplier_code = %s
-					AND docstatus = 1
-					AND date <= %s
-				ORDER BY date DESC
+					idr.category='Daily Rate'
+					AND idr.company = %s
+					AND idrt.item_code = %s
+					AND idrt.supplier_code = %s
+					AND idr.docstatus = 1
+					AND idr.date <= %s
+				ORDER BY idr.rate_date DESC
 				LIMIT 1
 			""", (args.company, item_doc.name, args.supplier, frappe.utils.now()), as_dict=True)
 
@@ -604,15 +606,17 @@ def get_price_list_rate(args, item_doc, out):
 		elif args.parenttype == 'Purchase Order' or args.doctype == 'Purchase Order':
 			# Combined query for 'Buying Rate' and 'Fresh Item Rate'
 			rate = frappe.db.sql("""
-				SELECT new_rate FROM `tabItem Daily Rate Table`
+				SELECT idrt.new_rate 
+					FROM `tabItem Daily Rate Table` AS idrt
+					JOIN `tabItem Daily Rate` AS idr ON idr.name = idrt.parent
 				WHERE
-					category IN ('Buying Rate','Fresh Item Rate')
-					AND company = %s
-					AND item_code = %s
-					AND supplier_code = %s
-					AND docstatus = 1
-					AND date <= %s
-				ORDER BY date DESC
+					idr.category IN ('Buying Rate','Fresh Item Rate')
+					AND idr.company = %s
+					AND idrt.item_code = %s
+					AND idrt,supplier_code = %s
+					AND idr.docstatus = 1
+					AND idr.rate_date <= %s
+				ORDER BY idr.rate_date DESC
 				LIMIT 1
 			""", (args.company, item_doc.name, args.supplier, frappe.utils.now()), as_dict=True)
 
@@ -620,15 +624,16 @@ def get_price_list_rate(args, item_doc, out):
 			oblige_rate = flt(rate[0].new_rate) if rate else None
 		elif args.parenttype == 'Material Request' or args.doctype == 'Material Request':
 			rate = frappe.db.sql("""
-				SELECT new_rate FROM `tabItem Daily Rate Table`
+				SELECT idrt.new_rate 
+					FROM `tabItem Daily Rate Table` AS idrt
 				WHERE
-					category IN ('Buying Rate','Fresh Item Rate')
-					AND company = %s
-					AND item_code = %s
-					AND supplier_code = %s
-					AND docstatus = 1
-					AND date <= %s
-				ORDER BY date DESC
+					idr.category IN ('Buying Rate','Fresh Item Rate')
+					AND idr.company = %s
+					AND idrt.item_code = %s
+					AND idrt.supplier_code = %s
+					AND idr.docstatus = 1
+					AND idr.rate_date <= %s
+				ORDER BY idr.rate_date DESC
 				LIMIT 1
 			""", (args.company, item_doc.name, args.supplier, frappe.utils.now()), as_dict=True)
 
