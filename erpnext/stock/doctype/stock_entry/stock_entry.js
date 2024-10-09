5eb8beea901378a175e02015e1d2333016cb4651
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2024-10-09 20:37:31 +0500

    Damage Return Validations

--- erpnext/stock/doctype/stock_entry/stock_entry.js
+++ erpnext/stock/doctype/stock_entry/stock_entry.js
@@ -537,7 +537,7 @@ if (frm.doc.docstatus === 0) {
 		debugger;
 
 		// Code by Moeiz for WIP Damage Return to disable fields
-		if(frm.doc.purpose == "Return WIP Damage") {
+		if(frm.doc.stock_entry_type == "Return WIP Damage") {
 			frm.set_df_property("fg_completed_qty", "hidden", 1);
 			frm.set_df_property("company", "read_only", 1);
 			frm.set_df_property("stock_entry_type", "read_only", 1);
