4a32efee7e97079546b3a45acc0759bd10c40f51
abdulsamad2929 <asramzani@gmail.com>
2024-11-01 10:45:04 +0500

    cash flow report optimization & indexing Accounts

--- erpnext/accounts/report/cash_flow/cash_flow.py
+++ erpnext/accounts/report/cash_flow/cash_flow.py
@@ -131,11 +131,12 @@ def get_account_type_based_gl_data(company, start_date, end_date, account_type, 
 
 	if filters.include_default_book_entries:
 		company_fb = frappe.db.get_value("Company", company, 'default_finance_book')
-		cond = """ AND (finance_book in (%s, %s, '') OR finance_book IS NULL)
-			""" %(frappe.db.escape(filters.finance_book), frappe.db.escape(company_fb))
+		# cond = """ AND (finance_book in (%s, %s, '') OR finance_book IS NULL)
+		# 	""" %(frappe.db.escape(filters.finance_book), frappe.db.escape(company_fb))
+		cond =""
 	else:
-		cond = " AND (finance_book in (%s, '') OR finance_book IS NULL)" %(frappe.db.escape(cstr(filters.finance_book)))
-
+		#cond = " AND (finance_book in (%s, '') OR finance_book IS NULL)" %(frappe.db.escape(cstr(filters.finance_book)))
+		cond =""
 
 	gl_sum = frappe.db.sql_list("""
 		select sum(credit) - sum(debit)
