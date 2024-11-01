5df78b2c0a3e965c91dddf12c8780c558f438007
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2024-11-01 14:36:28 +0500

    Asset Gross Value calculated on asset submission

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -31,6 +31,9 @@ class Asset(AccountsController):
 		# Code by Moeiz to validate company cost center and accounts
 		validate_company_cost_center_and_accounts(self)
 
+	def submit(self):
+		self.asset_gross_value = self.gross_purchase_amount + self.asset_capitalized_amount	
+
 	def on_submit(self):
 		self.validate_in_use_date()
 		self.set_status()
