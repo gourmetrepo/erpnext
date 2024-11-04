3458a8e7060460f1db87c34e6ffef6876dbd453a
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2024-11-04 16:58:43 +0500

    Ticket 126030: Maintenance Plant Development

--- erpnext/assets/doctype/maintenance/maintenance.js
+++ erpnext/assets/doctype/maintenance/maintenance.js
@@ -5,4 +5,38 @@ frappe.ui.form.on('Maintenance', {
 	// refresh: function(frm) {
 
 	// }
+
+
+	// workflow_state: function(frm){
+	// 	debugger;
+	// 	if (frm.doc.workflow_state == "CIP InProgress") {
+	// 		mark_cip_inprogress(frm);
+	// 	}else if (frm.doc.workflow_state == "CIP Finished") {
+	// 		mark_cip_finished(frm);
+	// 	}
+	// },
+
+	// mark_cip_inprogress: function(frm) {
+	// 	frappe.call({
+	// 		method: "mark_cip_inprogress",
+	// 		callback: function(r) {
+	// 			if(r.message) {
+	// 				frm.set_value("status", r.message);
+	// 				frm.reload_doc();
+	// 			}
+	// 		}
+	// 	});
+	// },
+
+	// mark_cip_finished: function(frm) {
+	// 	frappe.call({
+	// 		method: "mark_cip_finished",
+	// 		callback: function(r) {
+	// 			if(r.message) {
+	// 				frm.set_value("status", r.message);
+	// 				frm.reload_doc();
+	// 			}
+	// 		}
+	// 	});
+	// }
 });
--- erpnext/assets/doctype/maintenance/maintenance.py
+++ erpnext/assets/doctype/maintenance/maintenance.py
@@ -3,8 +3,33 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnext.manufacturing.doctype.work_order.work_order import stop_unstop
 
 class Maintenance(Document):
-	pass
+
+	def validate(self):
+		# Store the current value of workflow_state before any updates
+		self.previous_workflow_state = frappe.db.get_value("Maintenance", self.name, "workflow_state")
+	
+	def on_update(self):
+		# Check if workflow_state has changed
+		if self.previous_workflow_state and self.previous_workflow_state != self.workflow_state:
+			# Call the respective function based on the new workflow_state value
+			if self.workflow_state == "CIP Inprogress":
+				self.mark_cip_inprogress()
+			elif self.workflow_state == "CIP Finished":
+				self.mark_cip_finished()
+		else:
+			print("Hello")
+
+	def mark_cip_inprogress(self):
+		# Stop the work order if CIP document goes in progress
+		if self.work_order_id:
+			stop_unstop(self.work_order_id, "Stopped")
+
+	def mark_cip_finished(self):
+		# Resume the work order if CIP document is finished
+		if self.work_order_id:
+			stop_unstop(self.work_order_id, "Resumed")
