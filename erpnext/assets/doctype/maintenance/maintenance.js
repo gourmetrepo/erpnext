597bb2ef869b478b2908fdc4c3a38abb08eed5fc
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2024-11-06 12:48:57 +0500

    Ticket 126030: Maintenance Plant Development

--- erpnext/assets/doctype/maintenance/maintenance.js
+++ erpnext/assets/doctype/maintenance/maintenance.js
@@ -1,42 +1,127 @@
-// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
+// // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// // For license information, please see license.txt
+
+// frappe.ui.form.on('Maintenance', {
+
+
+
+// 	validate: function(frm) {
+// 		debugger;
+		
+// 		if (frm.doc.workflow_state == "CIP Inprogress"){
+// 		// First frappe.call
+// 		frappe.call({
+// 			method: "mark_cip_inprogress",
+// 			doc: frm.doc,
+// 			callback: function(r) {
+// 				if (r.message == "CIP Inprogress") {
+// 					console.log("Work Order Stopped");
+	
+// 					// Second frappe.call nested inside the first
+// 					if (frm.doc.cip_type == "Flavour Change") {
+// 						frappe.call({
+// 							method: "nrp_manufacturing.modules.gourmet.work_order.work_order.close_work_order",
+// 							args: {
+// 								work_order: frm.doc.work_order_id,
+// 								status: "Closed"
+// 							},
+// 							callback: function(r) {
+// 								if (r.message) {
+// 									let stock_entry = r.message;
+// 									if (isEmpty(stock_entry)) {
+// 										location.reload();
+// 									} else {
+// 										frappe.model.sync(stock_entry);
+// 										frappe.set_route('Form', stock_entry.doctype, stock_entry.name);
+// 									}
+// 								}
+// 							}
+// 						});
+// 					}
+// 				}
+// 			}
+// 		});
+// 	}
+
+// 	}
+	
+
+
+// 	// refresh: function(frm) {
+
+// 	// }
+
+
+// 	// workflow_state: function(frm){
+// 	// 	debugger;
+// 	// 	console.log("Workflow state changed", frm.doc.workflow_state)
+// 	// },
+
+// 	// mark_cip_inprogress: function(frm) {
+// 	// 	frappe.call({
+// 	// 		method: "mark_cip_inprogress",
+// 	// 		callback: function(r) {
+// 	// 			if(r.message) {
+// 	// 				frm.set_value("status", r.message);
+// 	// 				frm.reload_doc();
+// 	// 			}
+// 	// 		}
+// 	// 	});
+// 	// },
+
+// 	// mark_cip_finished: function(frm) {
+// 	// 	frappe.call({
+// 	// 		method: "mark_cip_finished",
+// 	// 		callback: function(r) {
+// 	// 			if(r.message) {
+// 	// 				frm.set_value("status", r.message);
+// 	// 				frm.reload_doc();
+// 	// 			}
+// 	// 		}
+// 	// 	});
+// 	// }
+// });
 
 frappe.ui.form.on('Maintenance', {
-	// refresh: function(frm) {
+	
+    refresh: function(frm) {
+        // Check if the workflow_state has changed
+        if (frm.doc.workflow_state === "CIP Inprogress" && frm.doc.previous_workflow_state !== frm.doc.workflow_state) {
+            // Run your first frappe.call
+            frappe.call({
+                method: "mark_cip_inprogress",
+                doc: frm.doc,
+                callback: function(r) {
+                    if (r.message === "CIP Inprogress") {
+                        
+						// Update previous workflow state to the current workflow_state
+						frm.set_value("previous_workflow_state", frm.doc.workflow_state);
+						frm.save();
+                        if (frm.doc.cip_type === "Flavour Change" || frm.doc.cip_type === "Pack Change") {
+                            frappe.call({
+                                method: "nrp_manufacturing.modules.gourmet.work_order.work_order.close_work_order",
+                                args: {
+                                    work_order: frm.doc.work_order_id,
+                                    status: "Closed"
+                                },
+                                callback: function(r) {
+                                    if (r.message) {
+                                        let stock_entry = r.message;
+                                        if (isEmpty(stock_entry)) {
+                                            location.reload();
+                                        } else {
+                                            frappe.model.sync(stock_entry);
+                                            frappe.set_route('Form', stock_entry.doctype, stock_entry.name);
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+            });
 
-	// }
 
-
-	// workflow_state: function(frm){
-	// 	debugger;
-	// 	if (frm.doc.workflow_state == "CIP InProgress") {
-	// 		mark_cip_inprogress(frm);
-	// 	}else if (frm.doc.workflow_state == "CIP Finished") {
-	// 		mark_cip_finished(frm);
-	// 	}
-	// },
-
-	// mark_cip_inprogress: function(frm) {
-	// 	frappe.call({
-	// 		method: "mark_cip_inprogress",
-	// 		callback: function(r) {
-	// 			if(r.message) {
-	// 				frm.set_value("status", r.message);
-	// 				frm.reload_doc();
-	// 			}
-	// 		}
-	// 	});
-	// },
-
-	// mark_cip_finished: function(frm) {
-	// 	frappe.call({
-	// 		method: "mark_cip_finished",
-	// 		callback: function(r) {
-	// 			if(r.message) {
-	// 				frm.set_value("status", r.message);
-	// 				frm.reload_doc();
-	// 			}
-	// 		}
-	// 	});
-	// }
+        }
+    }
 });
--- erpnext/assets/doctype/maintenance/maintenance.py
+++ erpnext/assets/doctype/maintenance/maintenance.py
@@ -9,27 +9,37 @@ from erpnext.manufacturing.doctype.work_order.work_order import stop_unstop
 
 class Maintenance(Document):
 
-	def validate(self):
-		# Store the current value of workflow_state before any updates
-		self.previous_workflow_state = frappe.db.get_value("Maintenance", self.name, "workflow_state")
+	# def validate(self):
+	# 	# Store the current value of workflow_state before any updates
+	# 	self.previous_workflow_state = frappe.db.get_value("Maintenance", self.name, "workflow_state")
 	
-	def on_update(self):
-		# Check if workflow_state has changed
-		if self.previous_workflow_state and self.previous_workflow_state != self.workflow_state:
-			# Call the respective function based on the new workflow_state value
-			if self.workflow_state == "CIP Inprogress":
-				self.mark_cip_inprogress()
-			elif self.workflow_state == "CIP Finished":
-				self.mark_cip_finished()
-		else:
-			print("Hello")
 
+	
+	# def on_update(self):
+	# 	# Check if workflow_state has changed
+	# 	if self.previous_workflow_state and self.previous_workflow_state != self.workflow_state:
+	# 		# Call the respective function based on the new workflow_state value
+	# 		if self.workflow_state == "CIP Inprogress":
+	# 			self.mark_cip_inprogress()
+	# 		elif self.workflow_state == "CIP Finished":
+	# 			self.mark_cip_finished()
+		
+	# 	self.previous_workflow_state = self.workflow_state
+	
 	def mark_cip_inprogress(self):
 		# Stop the work order if CIP document goes in progress
-		if self.work_order_id:
+		if self.workflow_state == "CIP Inprogress" and self.work_order_id:
 			stop_unstop(self.work_order_id, "Stopped")
+		
+		return self.workflow_state
+
+
+	def before_submit(self):
+		self.mark_cip_finished()
 
 	def mark_cip_finished(self):
 		# Resume the work order if CIP document is finished
-		if self.work_order_id:
+		if self.work_order_id and self.workflow_state == "CIP Finished" and self.cip_type == "General":
 			stop_unstop(self.work_order_id, "Resumed")
+
+
