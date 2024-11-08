68d9c576617db3bcdac98047abaa09a55de27a7e
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2024-11-08 11:31:46 +0500

    Ticket 126030: Maintenance Plant Development

--- erpnext/assets/doctype/cip_standard_time_setup/cip_standard_time_setup.py
+++ erpnext/assets/doctype/cip_standard_time_setup/cip_standard_time_setup.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class CIPStandardTimeSetup(Document):
-	pass
+	def before_save(self):
+		if self.is_new():
+			existing_document = frappe.db.sql("""SELECT `name` FROM `tabCIP Standard Time Setup` WHERE `cip_type`=%s""", (self.cip_type))
+
+			if len(existing_document) > 0 and len(existing_document[0]) > 0:
+				frappe.throw("CIP Standard Time Setup for {} already exists".format(self.cip_type))
