547da0774b24c11379eb19285e3f196e70abd085
abdulsamad2929 <asramzani@gmail.com>
2024-11-21 20:30:48 +0500

    Chnage calculation for 0

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -96,22 +96,27 @@ class PurchaseInvoice(BuyingController):
                                     GROUP BY party '''.format(customer,party_type,company),as_dict=True)
 
                     if len(total_receivable) > 0:
-                        total_receivable_value = flt(total_receivable[0]['total_receivable'])
-                        self.customer_loan_deduction[0].total_recieveable = flt(total_receivable[0]['total_receivable'])
+                        if flt(total_receivable[0]['total_receivable']) != flt(self.customer_loan_deduction[0].total_recieveable):
+                            total_receivable_value = flt(total_receivable[0]['total_receivable'])
+                            self.customer_loan_deduction[0].total_recieveable = flt(total_receivable[0]['total_receivable'])
+                        else:
+                            total_receivable_value = self.customer_loan_deduction[0].total_recieveable
                     else:
-                        total_receivable_value = flt(self.customer_loan_deduction[0].total_recieveable)
+                        total_receivable_value = 0.00
+                        self.customer_loan_deduction[0].total_recieveable = 0.00
+                
+                if flt(total_receivable_value) > 0.00:
+                    if not self.customer_loan_deduction[0].total_payable:
+                        self.customer_loan_deduction[0].total_payable = self.rounded_total
+                        
+                    if flt(total_receivable_value) > flt(self.customer_loan_deduction[0].total_payable):
+                        self.customer_loan_deduction[0].allocated_amount = self.customer_loan_deduction[0].total_payable
+                    else:
+                        self.customer_loan_deduction[0].allocated_amount = total_receivable_value
 
-                if not self.customer_loan_deduction[0].total_payable:
-                    self.customer_loan_deduction[0].total_payable = self.rounded_total
-                    
-                if flt(total_receivable_value) > flt(self.customer_loan_deduction[0].total_payable):
-                    self.customer_loan_deduction[0].allocated_amount = self.customer_loan_deduction[0].total_payable
-                else:
-                    self.customer_loan_deduction[0].allocated_amount = total_receivable_value
-
-                self.customer_loan_deduction[0].net_payable = flt(self.customer_loan_deduction[0].total_payable) - flt(self.customer_loan_deduction[0].allocated_amount)
-                self.customer_loan_deduction[0].net_recieveable = flt(total_receivable_value) - flt(self.customer_loan_deduction[0].allocated_amount)
-                self.customer_loan_deduction[0].allocation_percentage = flt((flt(self.customer_loan_deduction[0].allocated_amount) / flt(total_receivable_value)) * 100)
+                    self.customer_loan_deduction[0].net_payable = flt(self.customer_loan_deduction[0].total_payable) - flt(self.customer_loan_deduction[0].allocated_amount)
+                    self.customer_loan_deduction[0].net_recieveable = flt(total_receivable_value) - flt(self.customer_loan_deduction[0].allocated_amount)
+                    self.customer_loan_deduction[0].allocation_percentage = flt((flt(self.customer_loan_deduction[0].allocated_amount) / flt(total_receivable_value)) * 100)
 
         if not self.on_hold:
             self.release_date = ""
