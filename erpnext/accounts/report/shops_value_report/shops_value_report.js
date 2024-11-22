4f9eb7d67769809102d0201e657d9e42b87c9083
abdulsamad2929 <asramzani@gmail.com>
2024-11-22 18:22:46 +0500

    Shops Value Report

--- erpnext/accounts/report/shops_value_report/shops_value_report.js
+++ erpnext/accounts/report/shops_value_report/shops_value_report.js
@@ -4,6 +4,21 @@
 
 frappe.query_reports["Shops Value Report"] = {
 	"filters": [
-
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"shop_date",
+			"label": __("Shop Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
 	]
 };
--- erpnext/accounts/report/shops_value_report/shops_value_report.py
+++ erpnext/accounts/report/shops_value_report/shops_value_report.py
@@ -5,5 +5,64 @@ from __future__ import unicode_literals
 # import frappe
 
 def execute(filters=None):
-	columns, data = [], []
+	if not filters: filters = {}
+	columns = get_columns(filters)
+	
+	from datetime import datetime, date, timedelta
+	starttime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+	today = filter.shop_date
+	yesterday = today - timedelta(days=1)
+	shop_value_account_config = get_config_by_name('shop_value_account_config')
+	for (unit,unit_accounts) in shop_value_account_config.items():
+		if unit==filters.Company:
+			data = frappe.db.sql(f"""SELECT  party as customer,c.customer_name,account,SUM(shop_value) as shop_value,GROUP_CONCAT(ref_doc) AS ref_doc  FROM
+(
+SELECT   party,account,SUM(credit)-SUM(debit) AS shop_value GROUP_CONCAT(CONCAT('''',Voucher_no, '''' )) AS ref_doc
+                    FROM `tabGL Entry` 
+                    WHERE account IN ({unit_accounts.get('accounts')})
+                    AND DATE(posting_date) ='{yesterday}'  AND company='{unit}' AND Voucher_type='Sales Invoice' AND party IN (SELECT DISTINCT customer FROM `tabCustomers Company Assignment` WHERE company='{unit}')
+                    GROUP BY  account, party
+                    
+                    UNION ALL
+                    SELECT  GROUP_CONCAT(CONCAT('''',Voucher_no, '''' )) AS ref_doc ,account, party , SUM(credit)-SUM(debit) AS shop_value 
+                    FROM `tabGL Entry` 
+                    WHERE account IN ({unit_accounts.get('accounts')})
+                    AND DATE(posting_date) ='{today}'  AND company='{unit}' AND name NOT LIKE "%ACC-SVJV%" AND Voucher_type='Journal Entry' AND party IN (SELECT DISTINCT customer FROM `tabCustomers Company Assignment` WHERE company='{unit}')
+                    GROUP BY  account, party
+                
+                    UNION ALL
+                    SELECT  GROUP_CONCAT(CONCAT('''',Voucher_no, '''' )) AS ref_doc ,account, party , SUM(credit)-SUM(debit) AS shop_value 
+                    FROM `tabGL Entry` 
+                    WHERE account IN ({unit_accounts.get('accounts')})
+                    AND DATE(posting_date) ='{today}'  AND company='{unit}' AND Voucher_type='Payment Entry' AND party IN (SELECT DISTINCT customer FROM `tabCustomers Company Assignment` WHERE company='{unit}')
+                    GROUP BY  account, party
+                
+                    UNION ALL
+                    SELECT  GROUP_CONCAT(CONCAT('''',Voucher_no, '''' )) AS ref_doc ,account, party , SUM(credit)-SUM(debit) AS shop_value 
+                    FROM `tabGL Entry` 
+                    WHERE account IN ({unit_accounts.get('ng_accounts')})
+                    AND DATE(posting_date) ='{today}' AND company='{unit}' AND Voucher_type='Payment Entry' AND party IN (SELECT DISTINCT customer FROM `tabCustomers Company Assignment` WHERE company='{unit}')
+                    GROUP BY  account, party
+                
+                    UNION ALL
+                    SELECT  GROUP_CONCAT(CONCAT('''',Voucher_no, '''' )) AS ref_doc ,account, party , SUM(credit)-SUM(debit) AS shop_value 
+                    FROM `tabGL Entry` 
+                    WHERE account IN ({unit_accounts.get('ng_accounts')})
+                    AND DATE(posting_date) ='{today}' AND company='{unit}' AND Voucher_type='Sales Invoice' AND party IN (SELECT DISTINCT customer FROM `tabCustomers Company Assignment` WHERE company='{unit}')
+                    GROUP BY  account, party
+                    ) AS a
+					INNER JOIN `tabCustomer` AS c ON a.party = c.`name`  
+                    GROUP BY party, account""",as_dict=True,debug=True)
+
+
+
 	return columns, data
+
+
+def get_columns(filters):
+	"""return columns based on filters"""
+
+	columns = [_("Customer") + ":Link/Customer:100"]  + [_("Customer Name") + "::150"] + [_("Account") + "::150"] + \
+ 				[_("Shop Value") + ":Float:120"] + [_("Ref Doc") + "::420"] 
+	
+	return columns
