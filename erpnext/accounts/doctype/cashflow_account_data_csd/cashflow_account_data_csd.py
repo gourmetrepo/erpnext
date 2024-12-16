be7f47acc7a0f7ac15576c6fb201b9530a1514bd
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2024-12-16 12:31:13 +0500

    CSD Cashflow Job Automated: Code by Moeiz

--- erpnext/accounts/doctype/cashflow_account_data_csd/cashflow_account_data_csd.py
+++ erpnext/accounts/doctype/cashflow_account_data_csd/cashflow_account_data_csd.py
@@ -8,18 +8,29 @@ from frappe import _, _dict
 from frappe.model.document import Document
 import json
 from datetime import datetime,timedelta
+#site confige import
+from nrp_manufacturing.utils import  get_config_by_name
 class Cashflowaccountdatacsd(Document):
 	pass
 
 @frappe.whitelist()
-def insertDataQueue(from_date,to_date,heads,companies):
+def insertDataQueue(from_date=None,to_date=None,companies=None):
 	try:
 		if not from_date:
 			from_date = datetime.now().strftime('%Y-%m-%d')
 		if not to_date:
 			to_date = datetime.now().strftime('%Y-%m-%d')
 
-		j_companies = json.loads(companies)
+		cashflow_config = get_config_by_name('CASH_FLOW_DATA_CONFIG_CSD')
+
+		if companies:
+			if not isinstance(companies, list):
+				j_companies = json.loads(companies)
+			else:
+				j_companies = companies
+		else:
+			j_companies = cashflow_config.keys()
+
 		companies_tuple = tuple(j_companies)
 
 		query = """
@@ -33,19 +44,22 @@ def insertDataQueue(from_date,to_date,heads,companies):
 		
 		from_date = datetime.strptime(from_date, '%Y-%m-%d')
 		to_date = datetime.strptime(to_date, '%Y-%m-%d')
-		
-		while from_date <= to_date:
-			frappe.enqueue(
-				"erpnext.accounts.doctype.cashflow_account_data_csd.cashflow_account_data_csd.insertData",
-				from_date=from_date.strftime('%Y-%m-%d'),
-				to_date=from_date.strftime('%Y-%m-%d'),
-				heads=heads,
-				companies=companies,
-				queue="long",
-				timeout=13000,
-				enqueue_after_commit=True
-			)
-			from_date += timedelta(days=1)
+
+
+	
+		for company in companies_tuple:
+			while from_date <= to_date:
+				# insertData(from_date=from_date.strftime('%Y-%m-%d'), to_date=from_date.strftime('%Y-%m-%d'), company=company, cashflow_config=cashflow_config)
+				frappe.enqueue(
+					"erpnext.accounts.doctype.cashflow_account_data_csd.cashflow_account_data_csd.insertData",
+					from_date=from_date.strftime('%Y-%m-%d'), 
+					to_date=from_date.strftime('%Y-%m-%d'), 
+					company=company, cashflow_config=cashflow_config,
+					queue="long",
+					timeout=13000,
+					enqueue_after_commit=True
+				)
+				from_date += timedelta(days=1)
 	except Exception as e:
 		title = "CSD Cash Flow Data"
 		traceback = frappe.get_traceback()
@@ -53,74 +67,71 @@ def insertDataQueue(from_date,to_date,heads,companies):
 
 
 @frappe.whitelist()
-def insertData(from_date,to_date,heads,companies):
+def insertData(from_date,to_date, company, cashflow_config):
 	from erpnext.accounts.report.general_ledger.general_ledger import get_data_with_opening_closing, get_gl_entries, initialize_gle_map,get_accountwise_gle
-	from nrp_manufacturing.utils import get_config_by_name
-	cashflow_config = get_config_by_name('CASH_FLOW_DATA_CONFIG_CSD')
-	
+
 	from datetime import datetime, timedelta
 	from_date = datetime.strptime(from_date, "%Y-%m-%d").date()	
 	to_date = datetime.strptime(to_date, "%Y-%m-%d").date()	
 	current_date = from_date
 	account_head_total = {}
-	companies = json.loads(companies)
-	while current_date <= to_date:
-		for company, company_account in cashflow_config.items():
-			if company in companies:
-				for a in company_account:
-					head = a.get('head')
-					if head in heads:
-						for b in a.get('accounts'):
-							account_title = b.get('title')
-							account_total = 0
-							account_opening = 0
-							account_closing = 0
-							for account in b.get('account'):
-								print(account)
-								data = frappe.db.sql(
-									f""" SELECT 
-											(SELECT SUM(debit)-SUM(credit) FROM `tabGL Entry` 
-											WHERE ACCOUNT = "{account}" AND company = '{company}' AND posting_date < '{current_date}') AS opening,
-											(SELECT SUM(debit)-SUM(credit) FROM `tabGL Entry` 
-											WHERE voucher_type!='Period Closing Voucher' and ACCOUNT = "{account}" AND company = '{company}' AND posting_date = '{current_date}') AS value """
-											,as_dict=True
-								)
-								if data:
-									opening_balance = data[0].opening if data[0].opening != None else 0
-									value = data[0].value if data[0].value != None else 0
-									closing_balance = opening_balance + value
-									#save doc
-									save_doc = {
-										'doctype':'Cashflow account data csd',
-										'head':head,
-										'company':company,
-										'account': str(account),
-										'date':current_date,
-										'opening': opening_balance,
-										'closing' : closing_balance,
-										'value' : value
-									}
-									frappe.get_doc(save_doc).save(ignore_permissions=True)
-									account_total += value
-									account_opening += opening_balance
-									account_closing += closing_balance
-							if account_title == 'GAIN/LOSS ON SALE OF ASSETS':
-								account_total = account_total * -1
-							
+	
 
-							#save doc
-							save_doc = {
-								'doctype':'Cashflow account data csd',
-								'head':head,
-								'company':company,
-								'account': str(account_title),
-								'date':current_date,
-								'opening': account_opening,
-								'closing' : account_closing,
-								'value' : account_total
-							}
-							frappe.get_doc(save_doc).save(ignore_permissions=True)
-						frappe.db.commit()
+	while current_date <= to_date:
+		company_account = cashflow_config.get(company)
+		for a in company_account:
+			head = a.get('head')
+			for b in a.get('accounts'):
+				account_title = b.get('title')
+				account_total = 0
+				account_opening = 0
+				account_closing = 0
+				for account in b.get('account'):
+					print(account)
+					data = frappe.db.sql(
+						f""" SELECT 
+								(SELECT SUM(debit)-SUM(credit) FROM `tabGL Entry` 
+								WHERE ACCOUNT = "{account}" AND company = '{company}' AND posting_date < '{current_date}') AS opening,
+								(SELECT SUM(debit)-SUM(credit) FROM `tabGL Entry` 
+								WHERE voucher_type!='Period Closing Voucher' and ACCOUNT = "{account}" AND company = '{company}' AND posting_date = '{current_date}') AS value """
+								,as_dict=True
+					)
+					if data:
+						opening_balance = data[0].opening if data[0].opening != None else 0
+						value = data[0].value if data[0].value != None else 0
+						closing_balance = opening_balance + value
+						#save doc
+						save_doc = {
+							'doctype':'Cashflow account data csd',
+							'head':head,
+							'company':company,
+							'account': str(account),
+							'date':current_date,
+							'opening': opening_balance,
+							'closing' : closing_balance,
+							'value' : value
+						}
+						frappe.get_doc(save_doc).save(ignore_permissions=True)
+						account_total += value
+						account_opening += opening_balance
+						account_closing += closing_balance
+				if account_title == 'GAIN/LOSS ON SALE OF ASSETS':
+					account_total = account_total * -1
+				
+
+				#save doc
+				save_doc = {
+					'doctype':'Cashflow account data csd',
+					'head':head,
+					'company':company,
+					'account': str(account_title),
+					'date':current_date,
+					'opening': account_opening,
+					'closing' : account_closing,
+					'value' : account_total
+				}
+				frappe.get_doc(save_doc).save(ignore_permissions=True)
+				frappe.db.commit()
 		current_date += timedelta(days=1)
 
 
@@ -131,3 +142,12 @@ def updateCashBankData():
 		sql = f"""update `tabCashflow account data csd` set opening = {d.opening}, closing = {d.closing} where account = 'BANKS' and date = '{d.date}'"""
 		frappe.db.sql(sql)
 		print(d)	
+
+
+
+
+# Code by Moeiz
+# Cron job for daily update of cashflow data
+@frappe.whitelist()
+def csd_cashflow_data_job():
+	insertDataQueue()
