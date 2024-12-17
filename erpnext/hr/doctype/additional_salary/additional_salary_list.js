d738b088d386c3afe98218c14edd4f66e15987c6
attiqdeveloper <attiqur.rehman@gourmetpakistan.com>
2024-12-17 18:49:15 +0500

    HR Tasks: Changes
    - Additional Salary
    - Loan posting date issue resolve
    - Salary slip series update

---
+++ erpnext/hr/doctype/additional_salary/additional_salary_list.js
@@ -0,0 +1,33 @@
+frappe.listview_settings['Additional Salary'] = {
+    onload: function(listview) {
+        listview.page.add_inner_button(__('Download Template'), function() {
+            const filePath = "/assets/erpnext/csv/Additional_Salary_Template_SM.csv";
+            const baseUrl = window.location.origin;
+            const fileUrl = baseUrl + filePath;
+            window.open(fileUrl, '_blank');
+        });
+        listview.page.add_inner_button(__('Import Data'), function() {
+            new frappe.ui.FileUploader({
+                allow_multiple: false,
+                on_success: function(file) {
+                    frappe.call({
+                        method: 'nerp.modules.gourmet.additional_salary.additional_salary.create_doc_from_import',
+                        args: {
+                            file_url: file.file_url
+                        },
+                        freeze: true,
+			            freeze_message: `<img src="/assets/erpnext/images/output-onlinegiftools.gif" style="width: 150px; height: 150px;" />`,
+                        callback: function(response) {
+                            if (response.message.success){
+                                frappe.msgprint(__(response.message.success));
+                            }
+                            else{
+                                frappe.msgprint(__(response.message.error));
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    }
+};
