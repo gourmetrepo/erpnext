10e736d962d4def6cf8213f6b3f4d2f6714f9b82
attiqdeveloper <attiqur.rehman@gourmetpakistan.com>
2024-12-19 19:14:15 +0500

    HR Task: Import functionality for SSA

--- erpnext/hr/doctype/salary_structure_assignment/salary_structure_assignment_list.js
+++ erpnext/hr/doctype/salary_structure_assignment/salary_structure_assignment_list.js
@@ -7,5 +7,34 @@ frappe.listview_settings['Salary Structure Assignment'] = {
 				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -30),frappe.datetime.get_today()]]
 			};
 		}
+		me.page.add_inner_button(__('Download Template'), function() {
+            const filePath = "/assets/erpnext/csv/Salary_Structure_Assignment.csv";
+            const baseUrl = window.location.origin;
+            const fileUrl = baseUrl + filePath;
+            window.open(fileUrl, '_blank');
+        });
+        me.page.add_inner_button(__('Import Data'), function() {
+            new frappe.ui.FileUploader({
+                allow_multiple: false,
+                on_success: function(file) {
+                    frappe.call({
+                        method: 'nerp.modules.gourmet.salary_structure_assignment.salary_structure_assignment.create_ssa_doc_from_import',
+                        args: {
+                            file_url: file.file_url
+                        },
+                        freeze: true,
+			            freeze_message: `<img src="/assets/erpnext/images/output-onlinegiftools.gif" style="width: 150px; height: 150px;" />`,
+                        callback: function(response) {
+                            if (response.message.success){
+                                frappe.msgprint(__(response.message.success));
+                            }
+                            else{
+                                frappe.msgprint(__(response.message.error));
+                            }
+                        }
+                    });
+                }
+            });
+        });
 	}
 };
