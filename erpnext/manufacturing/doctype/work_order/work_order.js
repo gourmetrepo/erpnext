d07fc70f7cec7c96dbf244aa768e8468d2cb3978
Eesa Wazir <eesa.wazir@gourmetpakistan.com>
2024-12-24 15:00:16 +0500

    Ticket 126030: Maintenance Plant Development - CIP QA - close button logic moved from custom script and updated for inprogress cip

--- erpnext/manufacturing/doctype/work_order/work_order.js
+++ erpnext/manufacturing/doctype/work_order/work_order.js
@@ -182,6 +182,50 @@ frappe.ui.form.on("Work Order", {
 				frm.trigger("make_bom");
 			});
 		}
+
+		// Close button code in Custom Script moved here. Stop showing Close button in Work Order if CIP is in progress on the Cost Center/ Production Line
+		if((cur_frm.doc.status == 'Completed' || cur_frm.doc.status == 'Stopped') && cur_frm.doc.closed != 1 ){
+			const csd_companies = ['Unit 5', 'Unit 8', 'Unit 11'];
+			if (csd_companies.includes(frm.doc.company)) {
+				frappe.call({
+					method: 'frappe.client.get_list',
+					args: {
+						doctype: 'Maintenance',
+						field: ['name'],
+						filters: {
+							workflow_state: 'CIP Inprogress',
+							cost_center: frm.doc.production_line
+						}
+					},
+					callback: function(r) {
+						if (r.message) {
+							if (r.message.length < 1) {
+								cur_frm.add_custom_button(__("Close"), function() {
+									frappe.call({
+										method: "nrp_manufacturing.modules.gourmet.work_order.work_order.close_work_order",
+										args: {
+											work_order: cur_frm.doc.name,
+											status : cur_frm.doc.status
+										},
+										callback: function(r) {
+											if(r.message) {
+												let stock_entry = r.message;
+												if(isEmpty(stock_entry)){
+												location.reload()
+												}else{
+													frappe.model.sync(stock_entry);
+													frappe.set_route('Form', stock_entry.doctype, stock_entry.name);
+												}
+											}
+										}
+									});
+								}).addClass("btn-primary");
+							}
+						}
+					}
+				});
+			}
+        }
 	},
 
 	make_job_card: function(frm) {
