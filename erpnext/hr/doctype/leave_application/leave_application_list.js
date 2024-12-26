c29aebba63002218534463d3ea04be332a13c775
attiqdeveloper <attiqur.rehman@gourmetpakistan.com>
2024-12-26 13:52:47 +0500

    HR Task: Add import functionality for leave application

--- erpnext/hr/doctype/leave_application/leave_application_list.js
+++ erpnext/hr/doctype/leave_application/leave_application_list.js
@@ -15,6 +15,36 @@ frappe.listview_settings['Leave Application'] = {
 				"employee_name":frappe.session.user_fullname,
 				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -30),frappe.datetime.get_today()]]
 			};
+
+			me.page.add_inner_button(__('Download Template'), function() {
+				const filePath = "/assets/erpnext/csv/Leave_Application.csv";
+				const baseUrl = window.location.origin;
+				const fileUrl = baseUrl + filePath;
+				window.open(fileUrl, '_blank');
+			});
+			me.page.add_inner_button(__('Import Data'), function() {
+				new frappe.ui.FileUploader({
+					allow_multiple: false,
+					on_success: function(file) {
+						frappe.call({
+							method: 'nerp.modules.gourmet.leave_application.leave_application.create_la_doc_from_import',
+							args: {
+								file_url: file.file_url
+							},
+							freeze: true,
+							freeze_message: `<img src="/assets/erpnext/images/output-onlinegiftools.gif" style="width: 150px; height: 150px;" />`,
+							callback: function(response) {
+								if (response.message.success){
+									frappe.msgprint(__(response.message.success));
+								}
+								else{
+									frappe.msgprint(__(response.message.error));
+								}
+							}
+						});
+					}
+				});
+			});
 		}
 	}
 };
