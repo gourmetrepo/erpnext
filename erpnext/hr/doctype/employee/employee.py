59041253a9586e8f62e9088f2c4234306648a450
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2024-12-30 18:47:13 +0500

    Change HR Employee Tree Queue to Sync

--- erpnext/hr/doctype/employee/employee.py
+++ erpnext/hr/doctype/employee/employee.py
@@ -73,7 +73,7 @@ class Employee(NestedSet):
 
 
 	def on_update(self):
-		frappe.enqueue("frappe.utils.nestedset.update_nsm", doc=self, queue="long")
+		frappe.enqueue("frappe.utils.nestedset.update_nsm", doc=self, queue="sync")
 		if self.user_id:
 			self.update_user()
 			self.update_user_permissions()
