6b93c2c2ac06ce71c4facbfea41e2e6dbde64a35
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-01-07 18:09:21 +0500

    Enhancement:: Move js custom script to file code

--- erpnext/hr/doctype/job_applicant/job_applicant.js
+++ erpnext/hr/doctype/job_applicant/job_applicant.js
@@ -8,6 +8,11 @@ cur_frm.email_field = "email_id";
 
 frappe.ui.form.on("Job Applicant", {
 	refresh: function(frm) {
+		frappe.require("assets/nerp/js/jquery.maskedinput.min.js", () => {
+            $.mask.definitions['3'] = null;
+            $('input[data-fieldname="personal_cell_number"]').mask(frappe.utils.get_config_by_name('CELL_NUMBER_MASK','0399-9999999'),{autoclear: false});
+        });
+
 		if (!frm.doc.__islocal) {
 			if (frm.doc.__onload && frm.doc.__onload.job_offer) {
 				frm.add_custom_button(__("Job Offer"), function() {
--- erpnext/hr/doctype/loan_type/loan_type.js
+++ erpnext/hr/doctype/loan_type/loan_type.js
@@ -2,6 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Loan Type', {
-	refresh: function(frm) {
+	refresh(frm) {
+		frm.set_query("loan_account", "accounts", function(doc, cdt, cdn) {
+			var d = locals[cdt][cdn];
+			return {
+				filters: {
+					"is_group": 0,
+					"company": d.company
+				}
+			};
+		});
+		frm.set_query("interest_account", "accounts", function(doc, cdt, cdn) {
+			var d = locals[cdt][cdn];
+			return {
+				filters: {
+					"is_group": 0,
+					"company": d.company
+				}
+			};
+		});
 	}
 });
