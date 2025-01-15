bb329be180fedba7b33c8ee4d71dc3d607cf1bb5
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-01-15 15:22:09 +0500

    Merge Custom Script to Local JS

--- erpnext/hr/doctype/shift_request/shift_request.js
+++ erpnext/hr/doctype/shift_request/shift_request.js
@@ -2,13 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Shift Request', {
-	setup: function(frm) {
-		console.log(frm);
+    setup: function(frm) {
 		frappe.db.get_list("Employee", {
 			filters: { 'user_id': frappe.session.user},
 			fields: ["department"]
 		}).then((data) => {
-			if(data.length > 0){
+			if(data.length > 0){																																								
 				var departments = frappe.utils.get_config_by_name("SHIFT_REQUEST_RESTRICTED_DEPARTMENTS", []);
 				var allowed_shifts = frappe.utils.get_config_by_name("ALLOWED_SHIFT_REQUEST_FOR_RESTRICTED_DEPARTMENTS", []);
 				if( departments.indexOf(data[0].department) > -1 ) {
