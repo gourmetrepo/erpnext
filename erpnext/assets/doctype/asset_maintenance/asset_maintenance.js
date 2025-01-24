605dae3b349dabc512bd8df12b996d2b74c3ceb5
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2025-01-24 11:31:04 +0500

    FS-Plant Maintenance 2.0

--- erpnext/assets/doctype/asset_maintenance/asset_maintenance.js
+++ erpnext/assets/doctype/asset_maintenance/asset_maintenance.js
@@ -3,11 +3,7 @@
 
 frappe.ui.form.on('Asset Maintenance', {
 	setup: (frm) => {
-		frm.add_custom_button(__('Save'), function () {
-			frm.save();
-		}).addClass('btn-primary');
 
-		frm.page.btn_primary.addClass('hidden');
 		frm.set_indicator_formatter('status',
 			function(doc) {
 				let indicator = 'red'
@@ -38,11 +34,6 @@ frappe.ui.form.on('Asset Maintenance', {
 	},
 
 	refresh: (frm) => {
-		debugger;
-		frm.page.btn_primary.addClass('hidden');
-		frm.add_custom_button(__('Save'), function () {
-			frm.save();
-		}).addClass('btn-primary');
 		if(!frm.is_new()) {
 			frm.trigger('make_dashboard');
 		}
@@ -52,7 +43,6 @@ frappe.ui.form.on('Asset Maintenance', {
 	},
 
 	onload: (frm) => {
-		frm.page.btn_primary.addClass('hidden');
 		erpnext.asset_maintenance.manage_workflow_buttons(frm);
 		project_frm_configuration(frm);
 		
@@ -569,9 +559,6 @@ erpnext.asset_maintenance = {
 				frm.set_value('status', 'In Process');
 				frm.save();
 			}).addClass('btn-danger');
-			frm.add_custom_button(__('Save'), function () {
-				frm.save();
-			}).addClass('btn-primary');
 		} else if (status === "In Process") {
 			frm.add_custom_button(__('Consumption'), function () {
 				erpnext.asset_maintenance.make_material_consumption_stock_entry(frm);
@@ -586,17 +573,14 @@ erpnext.asset_maintenance = {
 				frm.set_value('status', 'Stopped');
 				frm.save();
 			}).addClass('btn-danger');
-			
-			frm.add_custom_button(__('Save'), function () {
-				frm.save();
-			}).addClass('btn-primary');
+
 		} else if (status === "Completed") {
 			frm.add_custom_button(__('Close'), function () {
 				erpnext.asset_maintenance.make_return_stock_entry(frm);
 			}).addClass('btn-danger');
 
 		} else if (status === "Closed"){
-			const buttons_to_remove = ['Start', 'Consumption', 'Completed', 'Save']
+			const buttons_to_remove = ['Start', 'Consumption', 'Completed']
 			for (var i = 0; i < buttons_to_remove.length; i++) {
 				this.frm.remove_custom_button(buttons_to_remove[i], __('Create'));
 			}
@@ -609,14 +593,6 @@ erpnext.asset_maintenance = {
 				erpnext.asset_maintenance.make_return_stock_entry(frm);
 			}).addClass('btn-danger');
 
-		} else if (status === "Draft") {
-			frm.add_custom_button(__('Save'), function () {
-				frm.save();
-			}).addClass('btn-primary');
-		} else {
-			frm.add_custom_button(__('Save'), function () {
-				frm.save();
-			}).addClass('btn-primary');
 		}
 
 	}
