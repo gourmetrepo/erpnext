eb6060b9f2d00642f8f8d5b4435705cb6011260a
abdulsamad2929 <asramzani@gmail.com>
2025-01-30 20:59:10 +0500

    Change for Budget

--- erpnext/accounts/doctype/budget/budget.py
+++ erpnext/accounts/doctype/budget/budget.py
@@ -57,9 +57,9 @@ class Budget(Document):
 				elif account_details.company != self.company:
 					frappe.throw(_("Account {0} does not belongs to company {1}")
 						.format(d.account, self.company))
-				elif account_details.report_type != "Profit and Loss":
-					frappe.throw(_("Budget cannot be assigned against {0}, as it's not an Income or Expense account")
-						.format(d.account))
+				# elif account_details.report_type != "Profit and Loss":
+				# 	frappe.throw(_("Budget cannot be assigned against {0}, as it's not an Income or Expense account")
+				# 		.format(d.account))
 
 				if d.account in account_list:
 					frappe.throw(_("Account {0} has been entered multiple times").format(d.account))
