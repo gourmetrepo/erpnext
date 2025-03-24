3292778597dcd2fccc2301e3b5979b0425ea7093
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-03-24 19:38:36 +0500

    add reference in dashboard

--- erpnext/assets/doctype/asset/asset_dashboard.py
+++ erpnext/assets/doctype/asset/asset_dashboard.py
@@ -1,23 +1,33 @@
 from __future__ import unicode_literals
 
 def get_data():
-	return {
-		'fieldname': 'asset_name',
-		'non_standard_fieldnames': {
-			'Asset Movement': 'asset'
-		},
-		'transactions': [
-			{
-				'label': ['Maintenance'],
-				'items': ['Asset Maintenance', 'Asset Maintenance Log']
-			},
-			{
-				'label': ['Repair'],
-				'items': ['Asset Repair']
-			},
-			{
-				'label': ['Movement'],
-				'items': ['Asset Movement']
-			}
-		]
-	}
+    return {
+        'fieldname': 'asset_name',
+        'non_standard_fieldnames': {
+            'Asset Movement': 'asset',
+            'Asset Capitalization': 'asset_id',
+            'Sales Invoice': 'asset'
+        },
+        'transactions': [
+            {
+                'label': 'Maintenance',
+                'items': ['Asset Maintenance', 'Asset Maintenance Log']
+            },
+            {
+                'label': 'Repair',
+                'items': ['Asset Repair']
+            },
+            {
+                'label': 'Movement',
+                'items': ['Asset Movement']
+            },
+            {
+                'label': 'Capitalization',
+                'items': ['Asset Capitalization']
+            },
+            {
+                'label': 'Sales Invoice',
+                'items': ['Sales Invoice']
+            }
+        ]
+    }
