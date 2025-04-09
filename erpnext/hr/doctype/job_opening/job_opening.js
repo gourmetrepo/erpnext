6ddae472f52a0460828d9bc62b36ea8649a8a374
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2025-04-09 12:12:41 +0500

    G-Recruitment changes

--- erpnext/hr/doctype/job_opening/job_opening.js
+++ erpnext/hr/doctype/job_opening/job_opening.js
@@ -1,74 +1,8 @@
-// Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+// Copyright (c) 2025, Shahzad Naser and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.handlers["Job Opening"]["designation"] = [];
-
-delete cur_frm.events["designation"];
-delete cur_frm.cscript["designation"];
-
 frappe.ui.form.on('Job Opening', {
-	onload: function(frm) {
-		frm.set_query("department", function() {
-			return {
-				"filters": {
-					"company": frm.doc.company,
-				}
-			};
-		});
+	// refresh: function(frm) {
 
-		frm.set_query("branch", function() {
-            if(!frm.doc.department){
-                frappe.msgprint("Please select Department first");
-            }
-            return {
-                "filters": {
-                    "department": frm.doc.department,
-                    "parent":["<","0"]
-                }
-            };
-        });
-        
-        frm.set_query("sub_branch", function() {
-            if(!frm.doc.branch){
-                frappe.msgprint("Please select Branch first");
-            }
-            return {
-                "filters": {
-                    "branch": frm.doc.branch
-                }
-            };
-        });
-	},
-	designation: function(frm) {
-		if(frm.doc.designation && frm.doc.company){
-			frappe.call({
-				"method": "erpnext.hr.doctype.staffing_plan.staffing_plan.get_active_staffing_plan_details",
-				args: {
-					company: frm.doc.company,
-					designation: frm.doc.designation,
-					date: frappe.datetime.now_date() // ToDo - Date in Job Opening?
-				},
-				callback: function (data) {
-					if(data.message){
-						frm.set_value('staffing_plan', data.message[0].name);
-						frm.set_value('planned_vacancies', data.message[0].vacancies);
-					} else {
-						frm.set_value('staffing_plan', "");
-						frm.set_value('planned_vacancies', 0);
-						frappe.show_alert({
-							indicator: 'orange',
-							message: __('No Staffing Plans found for this Designation')
-						});
-					}
-				}
-			});
-		}
-		else{
-			frm.set_value('staffing_plan', "");
-			frm.set_value('planned_vacancies', 0);
-		}
-	},
-	company: function(frm) {
-		frm.set_value('designation', "");
-	}
+	// }
 });
