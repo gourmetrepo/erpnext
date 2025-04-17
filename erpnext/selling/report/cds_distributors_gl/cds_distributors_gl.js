663ba1551c99550ff9d31a484be936dac79786fe
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-04-17 14:58:07 +0500

    CSD Distrubuter GL Report

--- erpnext/selling/report/cds_distributors_gl/cds_distributors_gl.js
+++ erpnext/selling/report/cds_distributors_gl/cds_distributors_gl.js
@@ -4,6 +4,40 @@
 
 frappe.query_reports["CDS Distributors GL"] = {
 	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 1,
+			"width": "80",
+			"on_change": function () {
+				frappe.query_report.get_filter("customer").set_value("");
+			}
+
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer",
+			"width": "80",
+			"get_query": function () {
+				let company = frappe.query_report.get_filter_value("company");
+				return {
+					filters: {
+						primary_company: company
+					}
+				};
+			}
+		},
+		{
+			"fieldname": "active",
+			"label": __("Active"),
+			"fieldtype": "Check",
+			"default": 1,
+			"width": "80"
+		}
 
 	]
 };
--- erpnext/selling/report/cds_distributors_gl/cds_distributors_gl.py
+++ erpnext/selling/report/cds_distributors_gl/cds_distributors_gl.py
@@ -1,9 +1,195 @@
-# Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
-# For license information, please see license.txt
-
 from __future__ import unicode_literals
-# import frappe
+import frappe
+
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+
+def get_columns():
+    return [
+        {
+            "label": "Customer Name",
+            "fieldname": "customer_name",
+            "fieldtype": "Data",
+            "width": 150,
+        },
+        {
+            "label": "Customer",
+            "fieldname": "customer",
+            "fieldtype": "Link",
+            "options": "Customer",
+            "width": 150,
+        },
+        {
+            "label": "Owner's CNIC",
+            "fieldname": "customer_cnic",
+            "fieldtype": "Data",
+            "width": 150,
+        },
+        {
+            "label": "CSD Distributors",
+            "fieldname": "csd_distributors",
+            "fieldtype": "Link",
+            "options": "Customer",
+            "width": 150,
+        },
+        {
+            "label": "CSD Distributor Security",
+            "fieldname": "csd_distributors_security",
+            "fieldtype": "Currency",
+            "width": 170,
+        },
+        {
+            "label": "VSP Distributors Amount",
+            "fieldname": "vsp_distributor_amount",
+            "fieldtype": "Currency",
+            "width": 180,
+        },
+        {
+            "label": "VSP Supplier Amount",
+            "fieldname": "vsp_supplier_amount",
+            "fieldtype": "Currency",
+            "width": 190,
+        },
+        {
+            "label": "CSD Distributor Amount",
+            "fieldname": "csd_distributor_amount",
+            "fieldtype": "Currency",
+            "width": 180,
+        },
+        {
+            "label": "Total Amount",
+            "fieldname": "total_amount",
+            "fieldtype": "Currency",
+            "width": 180,
+        },
+    ]
+
+
+def get_data(filters):
+    company = filters.get("company")
+    customer_filter = filters.get("customer")
+    active_filter = filters.get("active")
+
+    if not company:
+        return []
+
+    disabled_status = 0 if active_filter else 1
+
+    if customer_filter:
+        primary_customers = frappe.db.sql(
+            """
+			SELECT name, customer_name, cnic
+			FROM `tabCustomer`
+			WHERE primary_company = %s 
+			AND customer_group = 'CSD Distributors' 
+			AND name = %s 
+			AND disabled = %s
+		""",
+            (company, customer_filter, disabled_status),
+            as_dict=True,
+        )
+    else:
+        primary_customers = frappe.db.sql(
+            """
+			SELECT name, customer_name, cnic
+			FROM `tabCustomer`
+			WHERE primary_company = %s 
+			AND customer_group = 'CSD Distributors' 
+			AND disabled = %s
+		""",
+            (company, disabled_status),
+            as_dict=True,
+        )
+
+    if not primary_customers:
+        return []
+
+    primary_customer_names = (
+        [customer_filter] if customer_filter else [c.name for c in primary_customers]
+    )
+
+    secondary_customers = frappe.db.sql(
+        """
+		SELECT name, customer_name, cnic, primary_customer, customer_group
+		FROM `tabCustomer`
+		WHERE primary_customer IN %s
+	""",
+        (tuple(primary_customer_names),),
+        as_dict=True,
+    )
+
+    all_customers = {c["name"]: c for c in primary_customers}
+    for s in secondary_customers:
+        all_customers[s["name"]] = s
+
+    financials = frappe.db.sql(
+        """
+		SELECT party, SUM(debit) AS total_debit, SUM(credit) AS total_credit
+		FROM `tabGL Entry`
+		WHERE party_type = 'Customer' AND party IN %s
+		GROUP BY party
+	""",
+        (tuple(all_customers.keys()),),
+        as_dict=True,
+    )
+    fin_map = {f["party"]: f for f in financials}
+
+    data = []
+    for parent in primary_customers:
+        cust_name = parent["name"]
+        children = [
+            c for c in secondary_customers if c["primary_customer"] == cust_name
+        ]
+
+        vsp_distributor_total = 0
+        vsp_supplier_total = 0
+        csd_distributor_total = 0
+        csd_distributor_security = 0
+
+        parent_debit = fin_map.get(cust_name, {}).get("total_debit", 0)
+        parent_credit = fin_map.get(cust_name, {}).get("total_credit", 0)
+        parent_total = parent_credit - parent_debit
+        csd_distributor_total += parent_total
+
+        for child in children:
+            child_name = child["name"]
+            group = child.get("customer_group")
+            child_debit = fin_map.get(child_name, {}).get("total_debit", 0)
+            child_credit = fin_map.get(child_name, {}).get("total_credit", 0)
+            child_total = child_credit - child_debit
+
+            if group == "VSP Distributors":
+                vsp_distributor_total += child_total
+            elif group == "VSP Supplier":
+                vsp_supplier_total += child_total
+            elif group == "CSD Distributors":
+                csd_distributor_total += child_total
+            elif group == "CSD Distributor Security":
+                csd_distributor_security += child_total
+
+        overall_total = (
+            csd_distributor_total
+            + vsp_distributor_total
+            + vsp_supplier_total
+            + csd_distributor_security
+        )
+
+        data.append(
+            {
+                "customer_name": parent.get("customer_name"),
+                "customer": cust_name,
+                "customer_cnic": parent.get("cnic"),
+                "csd_distributors": cust_name,
+                "csd_distributors_security": csd_distributor_security,
+                "vsp_distributor_amount": vsp_distributor_total,
+                "vsp_supplier_amount": vsp_supplier_total,
+                "csd_distributor_amount": csd_distributor_total,
+                "total_amount": overall_total,
+            }
+        )
+
+    return data
