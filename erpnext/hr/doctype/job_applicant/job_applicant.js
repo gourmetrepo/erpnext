c1e64baea4681820a82230ba15acc0924c1a727f
gourmet-eesawazir <eesa.wazir@gourmetpakistan.com>
2025-04-25 12:21:41 +0500

    gRecruitment changes

--- erpnext/hr/doctype/job_applicant/job_applicant.js
+++ erpnext/hr/doctype/job_applicant/job_applicant.js
@@ -1,12 +1,41 @@
 
 frappe.ui.form.on("Job Applicant", {
-	
+	setup: function(frm) {
+		frm.fields_dict['core_skills'].grid.get_field('skill').get_query = function(doc, cdt, cdn) {
+            return {
+                filters: {
+                    'type': 'Functional'
+                }
+            };
+        };
+
+		frm.fields_dict['behavioral_competencies'].grid.get_field('skill').get_query = function(doc, cdt, cdn) {
+            return {
+                filters: {
+                    'type': 'Behavioral'
+                }
+            };
+        };
+	},	
+
 	refresh: function(frm) {
 		frappe.require("assets/nerp/js/jquery.maskedinput.min.js", () => {
             $.mask.definitions['3'] = null;
             $('input[data-fieldname="cnic"]').mask(frappe.utils.get_config_by_name('CNIC_MASK','99999-9999999-9'),{autoclear: false});
             $('input[data-fieldname="contact"]').mask(frappe.utils.get_config_by_name('CELL_NUMBER_MASK','0399-9999999'),{autoclear: false});
         });	
+	},
+
+    before_save: function(frm){
+		validate_email(frm.doc.email);
+
+		if (!frm.doc.truthful_information) {
+			frappe.throw(__("Mandatory fields required - I confirm that the information provided is accurate and truthful."));
+		}
+		
+		if (!frm.doc.personal_data_consent){
+			frappe.throw(__("Mandatory fields required - I consent to the processing of my personal data for recruitment purposes in accordance with applicable data protection laws."));
+		}
 	},
 
 	first_name: function(frm){
@@ -20,9 +49,29 @@ frappe.ui.form.on("Job Applicant", {
 	last_name: function(frm){
 		update_full_name(frm);
 	},
+
+    nationality: function(frm) {
+		if (frm.doc.nationality == 'Pakistan') {
+			frm.set_df_property("cnic", "reqd", 1);
+			frm.set_df_property("place_of_issue", "reqd", 1);
+			frm.set_df_property("issue_date", "reqd", 1);
+			frm.set_df_property("valid_upto", "reqd", 1);
+			frm.set_df_property("passport", "reqd", 0);
+			frm.set_df_property("passport_place_of_issue", "reqd", 0);
+			frm.set_df_property("passport_issue_date", "reqd", 0);
+			frm.set_df_property("passport_valid_upto", "reqd", 0);
+		} else {
+			frm.set_df_property("cnic", "reqd", 0);
+			frm.set_df_property("place_of_issue", "reqd", 0);
+			frm.set_df_property("issue_date", "reqd", 0);
+			frm.set_df_property("valid_upto", "reqd", 0);
+			frm.set_df_property("passport", "reqd", 1);
+			frm.set_df_property("passport_place_of_issue", "reqd", 1);
+			frm.set_df_property("passport_issue_date", "reqd", 1);
+			frm.set_df_property("passport_valid_upto", "reqd", 1);
+		}
+	},
 });
-
-
 
 
 function update_full_name(frm){
@@ -40,17 +89,36 @@ function update_full_name(frm){
 	frm.set_value("full_name", fullName);
 }
 
+function validate_email(email_address) {
+    const validEmail = frappe.utils.validate_type(email_address, "email");
+    if (!validEmail) {
+        frappe.throw(__("Please enter a valid email address"));
+    }
+}
 
 frappe.ui.form.on('Job Applicant Education', {
+	education_title: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.education_title) {
+			frappe.meta.get_docfield("Job Applicant Education", "type", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Education", "specialization", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Education", "institute", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Education", "start_date", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Education", "completion_date", frm.doc.name).reqd = 1;
+		}
+
+		frm.refresh_field("education");
+	},
+
     currently_enrolled: function(frm, cdt, cdn) {
         let row = locals[cdt][cdn];
-
         if (row.currently_enrolled) {
             frappe.model.set_value(cdt, cdn, 'completion_date', null); // optional: clear value
-
             frappe.meta.get_docfield("Job Applicant Education", "completion_date", frm.doc.name).hidden = 1;
+            frappe.meta.get_docfield("Job Applicant Education", "completion_date", frm.doc.name).reqd = 0;
         } else {
             frappe.meta.get_docfield("Job Applicant Education", "completion_date", frm.doc.name).hidden = 0;
+            frappe.meta.get_docfield("Job Applicant Education", "completion_date", frm.doc.name).reqd = 1;
         }
 
         frm.refresh_field("education");
@@ -59,17 +127,51 @@ frappe.ui.form.on('Job Applicant Education', {
 
 
 frappe.ui.form.on('Job Applicant Work Experience', {
+	job_title: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.job_title) {
+			frappe.meta.get_docfield("Job Applicant Work Experience", "company_name", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Work Experience", "location", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Work Experience", "joining_date", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Work Experience", "responsibilities_and_achievements", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Work Experience", "end_date", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Work Experience", "last_drawn_salary", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant Work Experience", "perks_and_benifits", frm.doc.name).reqd = 1;
+		}
+
+		frm.refresh_field("work_experience");
+	},
+
     currently_employed: function(frm, cdt, cdn) {
         let row = locals[cdt][cdn];
-
         if (row.currently_employed) {
             frappe.model.set_value(cdt, cdn, 'end_date', null); // optional: clear value
-
             frappe.meta.get_docfield("Job Applicant Work Experience", "end_date", frm.doc.name).hidden = 1;
+			frappe.meta.get_docfield("Job Applicant Work Experience", "end_date", frm.doc.name).reqd = 0;
         } else {
             frappe.meta.get_docfield("Job Applicant Work Experience", "end_date", frm.doc.name).hidden = 0;
+			frappe.meta.get_docfield("Job Applicant Work Experience", "end_date", frm.doc.name).reqd = 1;
         }
 
         frm.refresh_field("work_experience");
     }
+});
+
+frappe.ui.form.on('Job Applicant References', {
+	reference_full_name: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.reference_full_name) {
+			frappe.meta.get_docfield("Job Applicant References", "relationship", frm.doc.name).reqd = 1;
+			frappe.meta.get_docfield("Job Applicant References", "reference_contact", frm.doc.name).reqd = 1;
+		}
+
+		frm.refresh_field("references");
+	},
+
+	reference_email: function(frm, cdt, cdn) {
+		debugger;
+		if (frm.doc.reference_email) {
+			validate_email(frm.doc.reference_email);
+		}
+	}
 });
--- erpnext/hr/doctype/job_applicant/job_applicant.py
+++ erpnext/hr/doctype/job_applicant/job_applicant.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 import frappe
 from frappe import _
-from frappe.utils import comma_and, validate_email_address
+from frappe.utils import comma_and, flt, validate_email_address
 
 
 class JobApplicant(Document):
@@ -20,5 +20,17 @@ class JobApplicant(Document):
 	def before_save(self):
 		if not self.job_application_date:
 			self.job_application_date = frappe.utils.nowdate()
+		self.calculate_total_work_experience()
+
+	def calculate_total_work_experience(self):
+		total_experience = 0
+		if self.work_experience:
+			for we in self.work_experience:
+				diff = 0
+				if we.joining_date and we.end_date:
+					diff = frappe.utils.date_diff(we.end_date, we.joining_date)
+					if diff > 0:
+						total_experience += diff
+		self.total_work_experience_years = flt(total_experience / 365, 2)
 
 
