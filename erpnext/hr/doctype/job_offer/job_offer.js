91be35af6eeb614a824226810aeb829052d4e057
gourmet-eesawazir <eesa.wazir@gourmetpakistan.com>
2025-04-29 18:37:07 +0500

    gRecruitment changes

--- erpnext/hr/doctype/job_offer/job_offer.js
+++ erpnext/hr/doctype/job_offer/job_offer.js
@@ -1,5 +1,15 @@
 
 frappe.ui.form.on("Job Offer", {
+    setup: function(frm) {
+        frm.set_query("terms_and_conditions", function() {
+            return {
+                filters: {
+                    hr: 1
+                }
+            };
+        });
+    },
+
 	applicant_id: function(frm) {
         if (frm.doc.temp_applied_on) {
             frm.set_query('job_opening_id', function(doc) {
@@ -10,5 +20,5 @@ frappe.ui.form.on("Job Offer", {
                     };
             });
         }
-    },
+    }
 });
