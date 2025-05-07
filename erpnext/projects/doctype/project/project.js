3cedbe5164aa0177e567702569bd70edc62140f5
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2025-05-07 17:04:31 +0500

    Ticket#135243-Project Module Update - Ph3

--- erpnext/projects/doctype/project/project.js
+++ erpnext/projects/doctype/project/project.js
@@ -30,7 +30,7 @@ frappe.ui.form.on("Project", {
 		frm.set_query('parent_project', function () {
 			return {
 				filters: {
-					"is_parent_project": true
+					"is_group": true
 				}
 			};
 		});
@@ -95,7 +95,6 @@ frappe.ui.form.on("Project", {
 			frm.trigger('show_dashboard');
 		}
 		frm.events.set_buttons(frm);
-		parent_project_configuration(frm);
 	},
 
 	set_buttons: function(frm) {
@@ -195,11 +194,6 @@ frappe.ui.form.on("Project", {
 			frm.set_df_property('clearing_account', 'reqd', 0);
 			frm.set_df_property('clearing_account', 'hidden', 1);
 		}
-	},
-	
-
-	is_parent_project: function(frm) {
-		parent_project_configuration(frm);
 	}
 });
 
@@ -218,18 +212,4 @@ function open_form(frm, doctype, child_doctype, parentfield) {
 		frappe.ui.form.make_quick_entry(doctype, null, null, new_doc);
 	});
 
-}
-
-// Code by Moeiz
-// Confguration for parent and child project
-function parent_project_configuration(frm){
-	if (frm.doc.is_parent_project){
-		frm.set_df_property('parent_project', 'hidden', 0);
-		frm.set_df_property('parent_project', 'reqd', 1);
-	}else{
-		frm.set_df_property('parent_project', 'hidden', 1);
-		frm.set_df_property('parent_project', 'reqd', 0);
-		frm.set_value('parent_project', '');
-		frm.refresh_field('parent_project');
-	}
 }
