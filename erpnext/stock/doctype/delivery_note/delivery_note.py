d726d62a8ad3fb03aab7ccface575fe461a9283e
abdulsamad2929 <asramzani@gmail.com>
2025-05-08 18:07:54 +0500

    self.customer_name in case of making PO

--- erpnext/stock/doctype/delivery_note/delivery_note.py
+++ erpnext/stock/doctype/delivery_note/delivery_note.py
@@ -339,7 +339,7 @@ class DeliveryNote(SellingController):
 		#self.make_gl_entries()
 		frappe.db.sql("UPDATE `tabDelivery Note` SET queue_status='Completed' WHERE `name`='{docname}';".format(docname=self.name))
 		sale_order_type = frappe.db.get_value("Sales Order",self.sale_order_refrence,"order_type")
-		if sale_order_type == "Inter Unit Sales":
+		if sale_order_type == "Inter Unit Sales" and self.customer_name in ['Unit 5','Unit 8','Unit 11','Unit 17']:
 			make_purchase_order_interunit(self.name)
 			
 
