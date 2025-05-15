f53269b3519e809c14314470e51761c637da6b2e
attiqdeveloper <attiqur.rehman@gourmetpakistan.com>
2025-05-15 18:28:10 +0500

    Revert "gRecruitment changes"
    This reverts commit b5ddba4fef694842ee8fcd2a75735f8a628d4eab.

--- erpnext/hr/doctype/job_offer/job_offer.js
+++ erpnext/hr/doctype/job_offer/job_offer.js
@@ -1,45 +1,47 @@
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// License: GNU General Public License v3. See license.txt
+
+frappe.provide("erpnext.job_offer");
 
 frappe.ui.form.on("Job Offer", {
-    setup: function(frm) {
-        frm.set_query("terms_and_conditions", function() {
-            return {
-                filters: {
-                    hr: 1
-                }
-            };
-        });
-    },
-
-    onload: (frm) => {
-        frm.set_query('applicant_id', () => {
-			return {
-				filters: {
-					job_applicant_status: "Accepted"
-				}
-			};
+	onload: function (frm) {
+		frm.set_query("select_terms", function() {
+			return { filters: { hr: 1 } };
 		});
+	},
 
-    },
+	select_terms: function (frm) {
+		erpnext.utils.get_terms(frm.doc.select_terms, frm.doc, function (r) {
+			if (!r.exc) {
+				frm.set_value("terms", r.message);
+			}
+		});
+	},
 
-	applicant_id: function(frm) {
-        if (frm.doc.temp_applied_on) {
-            frm.set_query('job_opening_id', function(doc) {
-                return {
-                        filters: {
-                            "name": frm.doc.temp_applied_on
-                        }
-                    };
-            });
-        }
-    },
+	refresh: function (frm) {
+		if ((!frm.doc.__islocal) && (frm.doc.status == 'Accepted')
+			&& (frm.doc.docstatus === 1) && (!frm.doc.__onload || !frm.doc.__onload.employee)) {
+			frm.add_custom_button(__('Create Employee'),
+				function () {
+					erpnext.job_offer.make_employee(frm);
+				}
+			);
+		}
 
-    terms_and_conditions: function(frm) {
-        if (frm.doc.terms_and_conditions) {
-            frappe.db.get_value('Terms and Conditions', {'name': frm.doc.terms_and_conditions}, 'terms', (r) => {
-                if (r && r.terms) {
-                    frm.set_value('terms_and_conditions_text', r.terms);
-                }
-            });
-        }
-    }
+		if(frm.doc.__onload && frm.doc.__onload.employee) {
+			frm.add_custom_button(__('Show Employee'),
+				function () {
+					frappe.set_route("Form", "Employee", frm.doc.__onload.employee);
+				}
+			);
+		}
+	}
+
 });
+
+erpnext.job_offer.make_employee = function (frm) {
+	frappe.model.open_mapped_doc({
+		method: "erpnext.hr.doctype.job_offer.job_offer.make_employee",
+		frm: frm
+	});
+};
--- erpnext/hr/doctype/job_offer/job_offer.py
+++ erpnext/hr/doctype/job_offer/job_offer.py
@@ -10,26 +10,35 @@ from frappe import _
 from frappe.utils.data import get_link_to_form
 
 class JobOffer(Document):
-	def on_update_after_submit(self):
-		if self.applicant_id:
-			if self.offer_status == "Accepted":
-				frappe.db.set_value("Job Applicant", self.applicant_id, "job_applicant_status", "Offer Accepted")
-			elif self.offer_status == "Declined":
-				frappe.db.set_value("Job Applicant", self.applicant_id, "job_applicant_status", "Offer Rejected")
+	def onload(self):
+		employee = frappe.db.get_value("Employee", {"job_applicant": self.job_applicant}, "name") or ""
+		self.set_onload("employee", employee)
 
-	def before_save(self):
-		if self.is_new() and self.offer_status != "Offered":
-			frappe.throw(_("Job Offer can only be created with status <b>Offered<b>"))
+	def validate(self):
+		self.validate_vacancies()
 
-	def on_submit(self):
-		if self.applicant_id:
-			frappe.db.set_value("Job Applicant", self.applicant_id, "job_applicant_status", "Offered")
+	def validate_vacancies(self):
+		staffing_plan = get_staffing_plan_detail(self.designation, self.company, self.offer_date)
+		check_vacancies = frappe.get_single("HR Settings").check_vacancies
+		if staffing_plan and check_vacancies:
+			job_offers = self.get_job_offer(staffing_plan.from_date, staffing_plan.to_date)
+			if not staffing_plan.get("vacancies") or cint(staffing_plan.vacancies) - len(job_offers) <= 0:
+				error_variable = 'for ' + frappe.bold(self.designation)
+				if staffing_plan.get("parent"):
+					error_variable = frappe.bold(get_link_to_form("Staffing Plan", staffing_plan.parent))
 
-	def on_cancel(self):
-		self.offer_status = "Cancelled"
-		if self.applicant_id:
-			frappe.db.set_value("Job Applicant", self.applicant_id, "job_applicant_status", "Accepted")
+				frappe.throw(_("There are no vacancies under staffing plan {0}").format(error_variable))
 
+	def on_change(self):
+		update_job_applicant(self.status, self.job_applicant)
+
+	def get_job_offer(self, from_date, to_date):
+		''' Returns job offer created during a time period '''
+		return frappe.get_all("Job Offer", filters={
+				"offer_date": ['between', (from_date, to_date)],
+				"designation": self.designation,
+				"company": self.company
+			}, fields=['name'])
 
 def update_job_applicant(status, job_applicant):
 	if status in ("Accepted", "Rejected"):
