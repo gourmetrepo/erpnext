524e7b7bbf6a6b08eed65ac7e5811a7ab169fd6d
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-05-27 11:57:03 +0500

    Ticket:147054-Credit-Limit-Bank-Gurantee

--- erpnext/selling/doctype/customer/customer.js
+++ erpnext/selling/doctype/customer/customer.js
@@ -152,3 +152,14 @@ frappe.ui.form.on("Customer", {
 
 	},
 });
+
+frappe.ui.form.on('Customer Bank Guarantee', {
+	form_render(frm, cdt, cdn){
+		frm.fields_dict.bank_guarantee.grid.wrapper.find('.grid-delete-row').hide();
+        frm.fields_dict.bank_guarantee.grid.wrapper.find('.grid-duplicate-row').hide();
+        frm.fields_dict.bank_guarantee.grid.wrapper.find('.grid-move-row').hide();
+        frm.fields_dict.bank_guarantee.grid.wrapper.find('.grid-append-row').hide();
+        frm.fields_dict.bank_guarantee.grid.wrapper.find('.grid-insert-row-below').hide();
+        frm.fields_dict.bank_guarantee.grid.wrapper.find('.grid-insert-row').hide();
+	}
+});
