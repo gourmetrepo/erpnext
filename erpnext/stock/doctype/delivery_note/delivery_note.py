de43ad00725c6c9513daa66578b8380ac59f9a84
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-06-05 15:45:49 +0500

    add company in auto PO on inter unit

--- erpnext/stock/doctype/delivery_note/delivery_note.py
+++ erpnext/stock/doctype/delivery_note/delivery_note.py
@@ -352,7 +352,7 @@ class DeliveryNote(SellingController):
 		#self.make_gl_entries()
 		frappe.db.sql("UPDATE `tabDelivery Note` SET queue_status='Completed' WHERE `name`='{docname}';".format(docname=self.name))
 		sale_order_type = frappe.db.get_value("Sales Order",self.sale_order_refrence,"order_type")
-		if sale_order_type == "Inter Unit Sales" and self.customer_name in ['Unit 5','Unit 8','Unit 11','Unit 17'] and self.is_return == 0:
+		if sale_order_type == "Inter Unit Sales" and self.customer_name in ['Unit 5','Unit 8','Unit 11']  and self.company in ['Unit 5','Unit 8','Unit 11'] and self.is_return == 0:
 			make_purchase_order_interunit(self.name)
 			
 	def update_stock_reservation(self):
