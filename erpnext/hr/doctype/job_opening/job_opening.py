883666480409fc9fb70fffaa7246af20d50f892e
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-06-12 19:14:39 +0500

    Develop APIs for career portal

--- erpnext/hr/doctype/job_opening/job_opening.py
+++ erpnext/hr/doctype/job_opening/job_opening.py
@@ -10,13 +10,14 @@ from frappe import _
 from nerp.utils import get_config_by_name
 import requests
 import json
+from frappe.model.naming import make_autoname
+
 
 class JobOpening(Document):
-	def before_save(self):
-		if self.position:
-			position_title = frappe.get_value("Position", self.position, "position_title")
-			self.title = f"{position_title}"
+	def autoname(self):
+		self.title = f"{self.position_title}"
 
+	def before_save(self):
 		if self.is_new() and self.job_opening_status != "Open":
 			frappe.throw(_("Job Opening can only be created with status <b>Open<b>."))
 
