7f03a0bba470bdbb83022a74457a4470e1220458
attiqdeveloper <attiqur.rehman@gourmetpakistan.com>
2025-06-20 16:16:19 +0500

    Enhancement: Add provided mapping for production sub branches

--- erpnext/stock/report/stock_issuance_bakery_and_dairy/stock_issuance_bakery_and_dairy.py
+++ erpnext/stock/report/stock_issuance_bakery_and_dairy/stock_issuance_bakery_and_dairy.py
@@ -131,7 +131,11 @@ def get_conditions(filters):
 	if stock_entry_type:
 		conditions = f"{conditions} AND se.stock_entry_type = {frappe.db.escape(stock_entry_type)}"
 	if sub_branch:
-		conditions = f"{conditions} AND se.sub_branch = {frappe.db.escape(sub_branch)}"
+		if isinstance(sub_branch, list):
+			sub_branch_cond = 'se.sub_branch IN (' + ', '.join(f'"{sb}"' for sb in sub_branch) + ')'
+			conditions = f"{conditions} AND {sub_branch_cond}"
+		else:
+			conditions = f"{conditions} AND se.sub_branch = {frappe.db.escape(sub_branch)}"
 	if filters.get("posting_date"):
 		conditions = f"{conditions} AND se.posting_date >= {frappe.db.escape(filters.get('posting_date'))} AND se.posting_date <= {frappe.db.escape(filters.get('posting_date'))}"
 	if target_warehouse:
@@ -208,7 +212,92 @@ def get_department_mapping():
 					"ODS RSK RM - U6",
 					"ODS RSK PM - U6"
 				]
-			}
+			},
+			"Production-Issue": {
+				"Stock Entry Type": "Material Issue",
+				"Sub Branch": [
+					"1005 - Liaqat Chowk - U6",
+					"1008 - Youhannabad - U6",
+					"1010 - Wahdat Road - U6",
+					"1013 - Daroghawala - U6",
+					"1019 - Kot Abdul Malik - U6",
+					"1032 - Rahber Dha - U6",
+					"1037 - Raiwind Road - U6",
+					"1039 - FF-4 Dha - U6",
+					"1047 - Kahna Now - U6",
+					"1050 - Ferozpur Road - U6",
+					"1053 - Jaurrey Pull - U6",
+					"1061 - Military Accounts College Road Lhr - U6",
+					"1062 - Faisal Town - U6",
+					"1066 - Air Line - U6",
+					"1068 - 12-G - U6",
+					"1075 - Johar Town - U6",
+					"1099 - Eme Society - U6",
+					"1101 - Qadar Adab Chaowk Kasur - U6",
+					"2010 - Bankers Co-operative H.Society - U6",
+					"Auto Workshop - U6",
+					"Bakery Shipping Lahore Region - U6",
+					"Bakery Transportation Lahore Region - U6",
+					"Barfi - U6",
+					"Biscuit Finished Item - U6",
+					"Biscuit Mixing - U6",
+					"Biscuit Molding - U6",
+					"BON Cakes - U6",
+					"BON Sponge - U6",
+					"Bread Mixing - U6",
+					"Bread Molding - U6",
+					"Bread Oven - U6",
+					"Bread Packing - U6",
+					"Bulk Water - U6",
+					"Bulk Water Shipping - U6",
+					"Chum Chum - U6",
+					"Dry Cake Filling - U6",
+					"Dry Cake Mixing - U6",
+					"Dry Cake Molding - U6",
+					"Dry Cake Packing - U6",
+					"Executive Building - U6",
+					"FC Cakes - U6",
+					"FC Pastries - U6",
+					"FC Sponge - U6",
+					"Food Cooking U-3 - U6",
+					"Gate Office - U6",
+					"Jaman - U6",
+					"Kitchen Finished Item - U6",
+					"Kitchen Preparation - U6",
+					"Laddo - U6",
+					"Live Stock - U6",
+					"Material Store - U6",
+					"Nimko - U6",
+					"Oil & Lubricant - U6",
+					"Packing - U6",
+					"Paramedics - U6",
+					"Patisa - U6",
+					"Pizza Mixing - U6",
+					"Pizza Molding - U6",
+					"Pizza Oven - U6",
+					"Pizza Packing - U6",
+					"Pizza Preparation - U6",
+					"Production - U6",
+					"Puff Pastry Mixing - U6",
+					"Puff Pastry Molding - U6",
+					"Puff Pastry Oven - U6",
+					"Puff Pastry Packing - U6",
+					"Quality Assurance - U6",
+					"Quintech Sciences - U6",
+					"Research & Development - U6",
+					"Rusk Mixing - U6",
+					"Rusk Oven - U6",
+					"Rusk Packing - U6",
+					"Sanitation & Hygiene - U6",
+					"Supply Chain - U6",
+					"Unit Accounts - U6",
+					"Unit Administration - U6",
+					"Unit Electrical - U6",
+					"Unit Mechanical - U6",
+					"Unit Rac Refrigeration - U6",
+					"Unit Security - U6"
+				]
+			},
 		},
 		"Unit 6IC": {
 			"Production-Issue": {
