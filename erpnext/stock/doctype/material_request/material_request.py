72f0b48630f2b9447f2df8983364ffdac3a900dd
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2025-07-08 14:35:58 +0500

    Project based changes

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -626,7 +626,7 @@ def update_project_reference(project_id, docname):
 		frappe.db.sql(
 			f"""
 			UPDATE `tabPurchase Order Item` 
-			SET project = {frappe.db.escape(project_id)}
+			SET `project` = {frappe.db.escape(project_id)}
 			WHERE `material_request` = {frappe.db.escape(docname)}
 			"""
 		)
@@ -634,7 +634,7 @@ def update_project_reference(project_id, docname):
 		frappe.db.sql(
 			f"""
 			UPDATE `tabPurchase Order` 
-			SET project = {frappe.db.escape(project_id)}
+			SET `project_based` = 1
 			WHERE `name` in (select `parent` from `tabPurchase Order Item` where `material_request` = {frappe.db.escape(docname)})
 			"""
 		)
