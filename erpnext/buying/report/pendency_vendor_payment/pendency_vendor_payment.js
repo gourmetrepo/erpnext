48ca30656bbea29e6fd65a2d4efd65889155ddc5
abdulsamad2929 <asramzani@gmail.com>
2025-07-15 22:29:02 +0500

    filter set

--- erpnext/buying/report/pendency_vendor_payment/pendency_vendor_payment.js
+++ erpnext/buying/report/pendency_vendor_payment/pendency_vendor_payment.js
@@ -7,8 +7,7 @@ frappe.query_reports["Pendency Vendor Payment"] = {
 	{
 			fieldname: "company",
 			label: __("Company"),
-			// fieldtype: "Link",
-			 fieldtype: "MultiSelectList",
+			fieldtype: "Link",
 			options: "Company",
 			default: frappe.defaults.get_user_default("Company"),
 			reqd: 1
