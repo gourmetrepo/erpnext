88116ed0b6f8618222924036238fb28dd0ab237d
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2025-07-16 16:14:35 +0500

    Asset Maintenance dashboard updated

--- erpnext/assets/doctype/asset_maintenance/asset_maintenance_dashboard.py
+++ erpnext/assets/doctype/asset_maintenance/asset_maintenance_dashboard.py
@@ -4,13 +4,23 @@ from frappe import _
 def get_data():
 	return {
 		'fieldname': 'asset_maintenance',
+
 		'non_standard_fieldnames': {
 			'Journal Entry': 'plant_maintenance_reference',
 		},
+
+		'internal_links': {
+			'Material Request': ['bill_of_material_and_services', 'mr_reference']
+		},
+
 		'transactions': [
 			{
-				'label': _('Transactions'),
-				'items': ['Material Request','Stock Entry','Journal Entry']
+				'label': _('Procurement'),
+				'items': ['Material Request', 'Stock Entry']
+			},
+			{
+				'label': _('Accounting'),
+				'items': ['Journal Entry']
 			}
 		]
 	}
