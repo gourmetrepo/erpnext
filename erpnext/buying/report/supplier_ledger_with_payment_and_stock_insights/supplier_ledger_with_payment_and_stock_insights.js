ab91f878cdae9e0782437c2690d6b5412628b27d
abdulsamad2929 <asramzani@gmail.com>
2025-07-22 10:33:04 +0500

    Change report code

--- erpnext/buying/report/supplier_ledger_with_payment_and_stock_insights/supplier_ledger_with_payment_and_stock_insights.js
+++ erpnext/buying/report/supplier_ledger_with_payment_and_stock_insights/supplier_ledger_with_payment_and_stock_insights.js
@@ -4,6 +4,13 @@
 
 frappe.query_reports["Supplier Ledger with Payment and Stock Insights"] = {
 	"filters": [
-
+{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1
+		},
 	]
 };
--- erpnext/buying/report/supplier_ledger_with_payment_and_stock_insights/supplier_ledger_with_payment_and_stock_insights.py
+++ erpnext/buying/report/supplier_ledger_with_payment_and_stock_insights/supplier_ledger_with_payment_and_stock_insights.py
@@ -2,8 +2,188 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 
 def execute(filters=None):
 	columns, data = [], []
+
+	if not filters:
+		filters = {}
+
+	if filters.get("company") == "ALL":
+		company = f" and m.company IN ('Unit 5', 'Unit 8', 'Unit 11')"
+	else:
+		company = f""" and  m.company='{filters.get('company')}'"""
+	
+	from nrp_manufacturing.utils import  get_config_by_name
+	company_business_group = frappe.db.get_value('Company', filters.get("company"), 'business_group')
+	site_config = get_config_by_name('expense_entry_management_approval_date')
+	workflow_state = site_config.get(company_business_group)
+	
+	data = []
+	data = frappe.db.sql(f"""
+    SELECT * FROM (
+        SELECT 
+            pmodiffstock.*,
+            IFNULL( ROUND(SUM(gl.credit - gl.debit)), 0) AS gl_balance
+
+
+        FROM (
+            SELECT 
+                PMOdiff.posting_date,
+                PMOdiff.day_diff,
+                PMOdiff.company,
+                PMOdiff.ref_doc,
+                PMOdiff.supplier,
+                PMOdiff.supplier_name,
+				PMOdiff.mode_of_payment,
+                PMOdiff.amount,
+                PMOdiff.amount_paid,
+                ROUND(SUM(IFNULL(sle.actual_qty * sle.valuation_rate, 0))) AS stock_value
+
+            FROM (
+                SELECT 
+                    m.posting_date,
+                    CONCAT(
+                        TIMESTAMPDIFF(MONTH, m.posting_date, CURDATE()), 'M-',
+                        DATEDIFF(CURDATE(), m.posting_date) % 30, 'D'
+                    ) AS day_diff,
+                    m.company,
+                    CONCAT(
+                            "<a href='https://nrp.gourmetpakistan.com/desk#Form/Payment%20Order/", 
+                            d.parent, 
+                            "' target='_blank'>", 
+                            d.parent, 
+                            "</a>"
+                        ) AS ref_doc,
+
+                    d.supplier,
+                    d.supplier_name,
+					dr.mode_of_payment as mode_of_payment,
+                    ROUND(d.amount) AS amount,
+                    ROUND(d.amount_paid) AS amount_paid
+
+                FROM `tabPayment Order Detail` AS d
+                INNER JOIN `tabPayment Order` AS m ON m.name = d.parent
+				LEFT JOIN `tabPayment Order Reference` dr ON dr.`parent` = m.name AND d.supplier = dr.`supplier`
+                WHERE 
+                    d.amount_paid != d.amount
+                    AND m.docstatus = 1
+                    {company}
+
+                GROUP BY m.posting_date, m.company, d.supplier
+            ) AS PMOdiff
+
+            LEFT JOIN `tabBatch` AS btch 
+                ON PMOdiff.supplier = btch.supplier AND PMOdiff.company = btch.company
+
+            LEFT JOIN `tabStock Ledger Entry` AS sle 
+                ON btch.batch_id = sle.batch_no
+
+            GROUP BY PMOdiff.supplier, PMOdiff.company
+        ) AS pmodiffstock
+
+        LEFT JOIN `tabGL Entry` AS gl 
+            ON gl.party = pmodiffstock.supplier AND pmodiffstock.company = gl.company
+
+        LEFT JOIN `tabAccount` AS acc 
+            ON acc.name = gl.account AND acc.account_type IN ('Payable', 'Receivable')
+
+        GROUP BY pmodiffstock.supplier, pmodiffstock.company
+
+        UNION ALL
+        SELECT 
+            expdiffstock.*,
+           IFNULL( ROUND(SUM(gl.credit - gl.debit)), 0) AS gl_balance
+
+
+        FROM (
+            SELECT 
+                expdiff.posting_date,
+                expdiff.day_diff,
+                expdiff.company,
+                expdiff.ref_doc,
+                expdiff.supplier,
+                expdiff.supplier_name,
+				expdiff.mode_of_payment,
+                expdiff.amount,
+                expdiff.amount_paid,
+                ROUND(SUM(IFNULL(sle.actual_qty * sle.valuation_rate, 0))) AS stock_value
+
+            FROM (
+                SELECT 
+                    m.posting_date,
+                    CONCAT(
+                        TIMESTAMPDIFF(MONTH, m.posting_date, CURDATE()), 'M-',
+                        DATEDIFF(CURDATE(), m.posting_date) % 30, 'D'
+                    ) AS day_diff,
+                    m.company,
+                    CONCAT(
+                        "<a href='https://nrp.gourmetpakistan.com/desk#Form/Expense%20Entry/", 
+                        d.parent, 
+                        "' target='_blank'>", 
+                        d.parent, 
+                        "</a>"
+                    ) AS ref_doc,  d.party AS supplier,
+                    sup.supplier_name AS supplier_name,
+					m.mode_of_payment AS mode_of_payment,
+                    ROUND(d.amount) AS amount,
+                    0 AS amount_paid
+
+                FROM `tabExpense Entry Item` AS d
+                INNER JOIN `tabExpense Entry` AS m ON m.name = d.parent
+                INNER JOIN `tabSupplier` AS sup ON sup.name = d.party
+
+                WHERE 
+                    m.docstatus != 1
+                    AND m.management_approval_date IS NOT NULL
+                    {company}
+					AND m.workflow_state = '{workflow_state}'
+
+                GROUP BY m.posting_date, m.company, d.party
+            ) AS expdiff
+
+            LEFT JOIN `tabBatch` AS btch 
+                ON expdiff.supplier = btch.supplier AND expdiff.company = btch.company
+
+            LEFT JOIN `tabStock Ledger Entry` AS sle 
+                ON btch.batch_id = sle.batch_no
+
+            GROUP BY expdiff.supplier, expdiff.company
+        ) AS expdiffstock
+
+        LEFT JOIN `tabGL Entry` AS gl 
+            ON gl.party = expdiffstock.supplier AND expdiffstock.company = gl.company
+
+        LEFT JOIN `tabAccount` AS acc 
+            ON acc.name = gl.account AND acc.account_type IN ('Payable', 'Receivable')
+
+        GROUP BY gl.account, expdiffstock.supplier, expdiffstock.company
+
+    ) AS datanotpaid
+
+    ORDER BY day_diff DESC""", as_dict=True)
+
+	columns = get_columns(filters)
+	
 	return columns, data
+
+def get_columns(filters):
+		"""return columns based on filters"""
+	
+		columns = [
+		
+			{"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date",  "width": 120},
+			{"label": "Day Diff", "fieldname": "day_diff", "fieldtype": "Data",  "width": 120},
+			{"label": "Ref Doc", "fieldname": "ref_doc", "fieldtype": "Data", "width": 120},
+			{"label": "Supplier", "fieldname": "supplier", "fieldtype": "Data",  "width": 120},
+			{"label": "Supplier Name", "fieldname": "supplier_name", "fieldtype": "Data",  "width": 320},
+			{"label": "Mode of Payment", "fieldname": "mode_of_payment", "fieldtype": "Data",  "width": 120},
+			{"label": "Amount", "fieldname": "amount", "fieldtype": "Currency", "precision":"0", "align": "right", "width": 120},
+			{"label": "Amount Paid", "fieldname": "amount_paid", "fieldtype": "Currency", "precision":"0", "align": "right", "width": 120},
+			{"label": "Stock Value", "fieldname": "stock_value", "fieldtype": "Currency", "precision":"0", "align": "right", "width": 120},
+			{"label": "GL Balance", "fieldname": "gl_balance", "fieldtype": "Currency", "precision":"0", "align": "right", "width": 120}
+		]
+	
+
+		return columns
