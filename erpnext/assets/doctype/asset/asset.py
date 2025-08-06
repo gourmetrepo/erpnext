7bbba91000b6dd636ce1e88bd848c0fec40e6596
gourmet-eesawazir <eesa.wazir@gourmetpakistan.com>
2025-08-06 18:58:12 +0500

    fix: wrong variable name in asset

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -34,7 +34,7 @@ class Asset(AccountsController):
 		validate_company_cost_center_and_accounts(self)
 
 	def before_save(self):
-		self.asset_gross_value = self.gross_purchase_amount + self.asset_capitalized_amountz
+		self.asset_gross_value = self.gross_purchase_amount + self.asset_capitalized_amount
 
 	def on_submit(self):
 		self.validate_in_use_date()
