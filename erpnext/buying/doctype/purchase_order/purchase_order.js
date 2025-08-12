98c7f061486d0718241679fe83d5b509292928f3
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-08-12 12:00:56 +0500

    Restrict manual PO creation

--- erpnext/buying/doctype/purchase_order/purchase_order.js
+++ erpnext/buying/doctype/purchase_order/purchase_order.js
@@ -25,6 +25,12 @@ frappe.ui.form.on("Purchase Order", {
 				)
 			})
 		}
+		if (frm.is_new()) {
+            let restricted_companies = ["Unit 5", "Unit 8", "Unit 11"];            
+            if (restricted_companies.includes(frm.doc.company) && restricted_companies.includes(frm.doc.supplier_name)) {
+                frappe.throw("You are not allowed to create a document for inter.");
+            }
+        }
 	},
 	setup: function(frm) {
 
@@ -240,13 +246,6 @@ frappe.ui.form.on("Purchase Order", {
 		erpnext.queries.setup_queries(frm, "Warehouse", function() {
 			return erpnext.queries.warehouse(frm.doc);
 		});
-		if (frm.is_new()) {
-            let restricted_companies = ["Unit 5", "Unit 8", "Unit 11"];
-            
-            if (restricted_companies.includes(frm.doc.company) && frm.doc.supplier) {
-                frappe.throw("You are not allowed to create a document for this company.");
-            }
-        }
 	},
 
 
