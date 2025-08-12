53e4e47c3d15b98b9e3910a7ed52727c7887634c
Syed Shoaib Abbas <167745469+shoaibdevgourmet@users.noreply.github.com>
2025-08-12 11:12:47 +0500

    Add Restriction on frontend

--- erpnext/buying/doctype/purchase_order/purchase_order.js
+++ erpnext/buying/doctype/purchase_order/purchase_order.js
@@ -240,6 +240,13 @@ frappe.ui.form.on("Purchase Order", {
 		erpnext.queries.setup_queries(frm, "Warehouse", function() {
 			return erpnext.queries.warehouse(frm.doc);
 		});
+		if (frm.is_new()) {
+            let restricted_companies = ["Unit 5", "Unit 8", "Unit 11"];
+            
+            if (restricted_companies.includes(frm.doc.company) && frm.doc.supplier) {
+                frappe.throw("You are not allowed to create a document for this company.");
+            }
+        }
 	},
 
 
--- erpnext/buying/doctype/purchase_order/purchase_order.py
+++ erpnext/buying/doctype/purchase_order/purchase_order.py
@@ -50,8 +50,6 @@ class PurchaseOrder(BuyingController):
 			self.set_status()
 			self.check_on_hold_or_closed_status()
 			return
-
-		validate_inter_unit(self)
 		
 		# Code by Moeiz
 		# Project Based MR -> PO Validation
@@ -860,11 +858,7 @@ def validate_no_project_reference(doc):
 		for item in doc.items:
 			if item.project:
 				frappe.throw(f"Purchase Order is not project based. Please remove project in Purchase Order items in Row# {item.idx} or Mark Purchase Order (or relevant Material Request) as Project Based")
-		
-def validate_inter_unit(purchase_order):
-	"""Validate inter-unit purchase order for CSD Unit 5, Unit 8, Unit 11."""
-	if purchase_order.company in ['Unit 5', 'Unit 8', 'Unit 11'] and purchase_order.supplier_name in ['Unit 5', 'Unit 8', 'Unit 11']:
-		frappe.throw(_("Inter-unit purchase order is not allowed for CSD Unit 5, Unit 8, Unit 11"))
+
 
 @frappe.whitelist()
 def create_purchase_receipt_for_subcontractor(docname):
