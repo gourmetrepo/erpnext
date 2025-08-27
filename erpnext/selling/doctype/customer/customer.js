01068793ae16ef9750f1318bc5e7f1581e7a92bc
attiqdeveloper <attiqur.rehman@gourmetpakistan.com>
2025-08-27 21:06:01 +0500

    SCM: Add customer details from gssm into gerp

--- erpnext/selling/doctype/customer/customer.js
+++ erpnext/selling/doctype/customer/customer.js
@@ -146,9 +146,166 @@ frappe.ui.form.on("Customer", {
 		var grid = cur_frm.get_field("sales_team").grid;
 		grid.set_column_disp("allocated_amount", false);
 		grid.set_column_disp("incentives", false);
+
+		if (frm.doc.customer_group == "CSD Distributors") {
+            frappe.call({
+                method: "erpnext.selling.doctype.customer.customer.get_distribution_geo",
+                args: {
+                    distribution_code: frm.doc.name
+                },
+                callback: function(r) {
+                    if (r.message && r.message.length > 0) {
+                        let data = r.message[0];
+
+                        let html = `
+							<style>
+								.table-wrapper {
+									overflow: auto;
+									max-width: 100%;
+									max-height: 600px;
+									border: 1px solid #ccc;
+								}
+
+								.sticky-table {
+									border-collapse: collapse;
+									width: 100%;
+								}
+
+								/* Make all header cells sticky */
+								.sticky-table th {
+									position: sticky;
+									top: 0;
+									/* background: #fff; */
+									z-index: 1;
+									text-align: center;
+									border: 1px solid #ccc;
+								}
+
+								/* Second header row is below the first, so higher top offset */
+								.sticky-table tr:nth-child(2) th {
+									top: 34px; /* Adjust based on your first row height */
+								}
+
+								/* First column (Date) – both th and td */
+								.sticky-table th:first-child,
+								.sticky-table td:first-child {
+									position: sticky;
+									left: 0;
+									background: #f1f1f1;
+									z-index: 2;
+								}
+
+								/* Corner cell (top-left "Date") – must be on top */
+								.sticky-table tr:first-child th:first-child {
+									z-index: 5;
+								}
+
+								table {
+									width: 100%;
+									border-collapse: collapse;
+									background: white;
+									float:left;
+								}
+								.table{
+									margin:0px;
+								}
+								.tableviewmore th{
+									border: 1px solid #000 !important;
+								}
+								th, td {
+									border: 1px solid #000;
+									padding: 8px;
+									text-align: left;
+								}
+								th {
+									background-color: #ddd;
+								}
+								.header {
+									text-align: center;
+									font-weight: bold;
+									font-size: 18px;
+									padding: 10px;
+									background: #444;
+									color: white;
+									width:100%;
+									float:left;
+								}
+
+								.sub-header {
+									text-align: center;
+									font-weight: bold;
+									font-size: 14px;
+									padding: 10px;
+									background: #00b33c;
+									color: white;
+									width:100%;
+									float:left;
+								}
+								div.wrapper{
+									float:left;
+									width:100%;
+									overflow-y:auto;
+									max-height: 800px;
+									overflow-x: auto;
+								}
+								th{
+									text-align: center;
+								}
+								td a{
+									text-decoration: underline;
+								}
+								button.view_more{
+									margin-top:2px;
+									padding-top:0px;
+									padding-bottom:0px;
+								}
+								.table-inner{
+									margin:0;
+								}
+							</style>
+                            <table>
+								<tr>
+									<th>Region</th>
+									<th>Zone</th>
+									<th>Area</th>
+									<th>Territory</th>
+									<th>Warehouse Latitude</th>
+									<th>Warehouse Longitude</th>
+									<th>Distribution Latitude</th>
+									<th>Distribution Longitude</th>
+								</tr>
+								<tr>
+									<td>${data.region}</td>
+									<td>${data.zone}</td>
+									<td>${data.area}</td>
+									<td>${data.territory}</td>
+									<td>${data.warehouseLatitude}</td>
+									<td>${data.warehouseLongitude}</td>
+									<td>${data.distributionLatitude}</td>
+									<td>${data.distributionLongitude}</td>
+								</tr>
+                            </table>
+                        `;
+
+                        frm.fields_dict.distributor_details_html.$wrapper.html(html);
+                    } else {
+                        frm.fields_dict.distributor_details_html.$wrapper.html("<p>No distribution data found.</p>");
+                    }
+                }
+            });
+        }
 	},
 	validate: function(frm) {
 		if(frm.doc.lead_name) frappe.model.clear_doc("Lead", frm.doc.lead_name);
 
 	},
+	onload: function(frm) {
+        if (frm.doc.customer_group == "CSD Distributors") {
+            frm.fields_dict.distributor_details_html.$wrapper.html(
+                `<div style="padding:8px; background:#f9f9f9; border:1px solid #ddd; border-radius:5px;">
+                    <b>Note:</b> Please make sure to fill in the <i>NTN</i>, <i>STRN</i> and <i>Credit Limit</i> fields in the 'More Information' section.
+                </div>`
+            );
+        }
+    }
 });
