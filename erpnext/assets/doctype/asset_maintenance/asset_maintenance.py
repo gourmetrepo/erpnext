be74b8bf695443f4069aac14b5c9cc5cbbaabc04
gourmet-eesawazir <eesa.wazir@gourmetpakistan.com>
2025-08-27 20:31:29 +0500

    asset maintenance - fix condition

--- erpnext/assets/doctype/asset_maintenance/asset_maintenance.py
+++ erpnext/assets/doctype/asset_maintenance/asset_maintenance.py
@@ -61,7 +61,7 @@ class AssetMaintenance(Document):
 		if self.project_based == "No" and not self.clearing_account:
 			frappe.throw("Please set clearing account to proceed for non project based asset maintenance. Contact support team for more information.")
 
-		if self.status != "Closed" or self.status != "Finished":
+		if not (self.status == "Closed" or self.status == "Finished"):
 			frappe.throw("Asset Maintenance cannot be submitted if its status is not Closed or Finished.")
 		
 		# Create Damage and Scrap stock entries
