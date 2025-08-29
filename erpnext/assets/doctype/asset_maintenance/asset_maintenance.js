e941cf6e07ab24bd78b686bfba6ca3a161b386f1
gourmet-eesawazir <eesa.wazir@gourmetpakistan.com>
2025-08-29 18:02:21 +0500

    Revert "asset maintenance - close button is removed when process is complete (prevent multiple Close events)"
    This reverts commit 1cfa2e79af511366ccfb49534fb301bcaa8cd67b.

--- erpnext/assets/doctype/asset_maintenance/asset_maintenance.js
+++ erpnext/assets/doctype/asset_maintenance/asset_maintenance.js
@@ -220,7 +220,7 @@ frappe.ui.form.on('Asset Maintenance', {
 	},
 
 	get_project_tasks: (frm) => {
-		// frm.clear_table("asset_maintenance_tasks");
+
 
 		if (frm.is_dirty()) {
 			frappe.throw(__(`Save document before fetching project tasks`));
@@ -583,14 +583,7 @@ erpnext.asset_maintenance = {
 
 		} else if (status === "Completed") {
 			frm.add_custom_button(__('Close'), function () {
-				try {
-					erpnext.asset_maintenance.make_return_stock_entry(frm);
-					this.frm.remove_custom_button('Close', __('Create'));
-				} catch (error) {
-					frm.add_custom_button(__('Close'), function () {
-						erpnext.asset_maintenance.make_return_stock_entry(frm);
-					}).addClass('btn-danger');
-				}
+				erpnext.asset_maintenance.make_return_stock_entry(frm);
 			}).addClass('btn-danger');
 
 		} else if (status === "Closed"){
@@ -604,14 +597,7 @@ erpnext.asset_maintenance = {
 				frm.save();
 			}).addClass('btn-danger');
 			frm.add_custom_button(__('Close'), function () {
-				try {
-					erpnext.asset_maintenance.make_return_stock_entry(frm);
-					this.frm.remove_custom_button('Close', __('Create'));
-				} catch (error) {
-					frm.add_custom_button(__('Close'), function () {
-						erpnext.asset_maintenance.make_return_stock_entry(frm);
-					}).addClass('btn-danger');
-				}
+				erpnext.asset_maintenance.make_return_stock_entry(frm);
 			}).addClass('btn-danger');
 
 		}
