443bed2da28cc7cd7e66e828d5d4d2eb833dc0f1
abdulsamad2929 <asramzani@gmail.com>
2025-08-29 15:27:34 +0500

    Change query form GSSM DN sync

--- erpnext/stock/doctype/delivery_note/delivery_note.py
+++ erpnext/stock/doctype/delivery_note/delivery_note.py
@@ -347,7 +347,7 @@ class DeliveryNote(SellingController):
 		# stock_gl.save(ignore_permissions=True)
 		if self.request_from == "GSSM":
 			from nrp_manufacturing.utils import send_notification_to_gssm
-			send_notification_to_gssm(status=None, document_number=self.sale_order_refrence, document_type="Sales Order", vehicle=self.vehicle if self.vehicle else None, driver= self.driver if self.driver else None, delivery_note=self.name)
+			send_notification_to_gssm(status=None, document_number=self.sale_order_refrence, document_type="Sales Order", vehicle=self.vehicle_no if self.vehicle_no else None, driver= self.transporter_name if self.transporter_name else None, delivery_note=self.name)
 		
 		time.sleep(1)
 		try:
